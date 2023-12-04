45cb53e5ccf8163fda6b9bfef9f49f4de5294f5c
Nanowolf4 <n4w@tutanota.com>
2023-12-04 13:14:20 +0200

    replace random messages

--- mods/other/random_messages/init.lua
+++ mods/other/random_messages/init.lua
@@ -3,25 +3,16 @@ RandomMessages mod by arsdragonfly.
 arsdragonfly@gmail.com
 6/19/2013
 --]]
---Time between two subsequent messages.
-local MESSAGE_INTERVAL = 0
-local S = minetest.get_translator(minetest.get_current_modname())
 
 math.randomseed(os.time())
 
 random_messages = {}
 random_messages.messages = {} --This table contains all messages.
 
-function random_messages.initialize() --Set the interval in minetest.conf.
-	minetest.settings:set("random_messages_interval", 60)
-	minetest.settings:write();
-	return 60
-end
 
-function random_messages.set_interval() --Read the interval from minetest.conf and set it if it doesn't exist
-	MESSAGE_INTERVAL = tonumber(minetest.settings:get("random_messages_interval"))
-							or random_messages.initialize()
-end
+--Time between two subsequent messages.
+local MESSAGE_INTERVAL = tonumber(minetest.settings:get("random_messages_interval") or 120) 
+
 
 function random_messages.check_params(name,func,params)
 	local stat, msg = func(params)
@@ -34,60 +25,63 @@ end
 
 function random_messages.read_messages()
 	random_messages.messages = {
-		"To talk to only your team, start your messages with /t. For example, /t Hello team!",
-		"Use apples and blueberries to quickly restore your health.",
-		"Moving or fighting can avoid an inactivity kick.",
-		"Gain more score by killing more than you die, by healing teammates with bandages, or by capturing the flag.",
+		"To talk only with your team, start your messages with /t. For example, /t Hello team!",
+		"Use apples to quickly restore your health.",
+		"Moving or fighting can prevent an inactivity kick.",
+		"Earn more score by killing more than you die, healing teammates with bandages, or capturing the flag.",
 		"You gain more score the better the opponent you defeat.",
-		"Find weapons in chests or mine and use furnaces to make stronger swords.",
-		"Use team doors (steel) to stop the enemy walking into your base.",
-		"Sprint by pressing the fast key (E) when you have stamina.",
-		"Like CTF? Give feedback using /report, and consider donating at rubenwardy.com/donate",
-		"Want to submit your own map? Visit https://github.com/MT-CTF/maps to get involved.",
-		"Using limited resources for building structures that don't strengthen your base's defences is discouraged.",
-		"To report misbehaving players to moderators, please use /report <name> <action>",
-		"Swearing, trolling and being rude will not be tolerated and strict action will be taken.",
-		"Trapping team mates on purpose is strictly against the rules and you will be kicked immediately.",
+		"Find weapons in chests or mine and use furnaces to craft stronger swords.",
+		"Use team doors (steel) to prevent the enemy from entering your base.",
+		"Sprint by pressing the sprint key (E) when you have stamina.",
+		"Want to submit your own map? Visit https://discord.gg/KuUXcBuG5M to get involved.",
+		"Using limited resources to build structures that don't strengthen your base's defenses is discouraged.",
+		"To report misbehaving players to moderators, please use /report <name> <action> or send the report on Discord: ctf.jma-sig.de",
+		"Swearing, trolling, and being rude will not be tolerated, and strict action will be taken.",
+		"Trapping teammates on purpose is strictly against the rules, and you will be kicked immediately.",
 		"Help your team claim victory by storing extra weapons in the team chest, and never taking more than you need.",
 		"Excessive spawn-killing is a direct violation of the rules - appropriate punishments will be given.",
 		"Use /r to check your rank and other statistics.",
 		"Use /r <playername> to check the rankings of another player.",
-		"Use bandages on team-mates to heal them by 3-4 HP if their health is below 15 HP.",
-		"Use /m to add a team marker at pointed location, that's visible only to team-mates.",
+		"Use bandages on teammates to heal them by 3-4 HP if their health is below 15 HP.",
+		"Use /m to add a team marker at a pointed location, visible only to teammates.",
 		"Use /summary (or /s) to check scores of the current match and the previous match.",
 		"Strengthen your team by capturing enemy flags.",
 		"Hitting your enemy does more damage than not hitting them.",
 		"Use /top50 command to see the leaderboard.",
-		"Use /top50 <mode:technical modename> to see the leaderboard on another mode."
+		"Use /top50 <mode:technical modename> to see the leaderboard in another mode."
 		.. " For example: /top50 mode:nade_fight.",
-		"To check someone's rank on another mode, use /r <mode:technical modename> <playername>."
+		"To check someone's rank in another mode, use /r <mode:technical modename> <playername>."
 		.. " For example: /r mode:nade_fight randomplayer.",
-		"To check someone's team use /team player <player_name>.",
-		"To check all team members use /team.",
+		"To check someone's team, use /team player <player_name>.",
+		"To check all team members, use /team.",
 		"You can capture multiple enemy flags at once!",
-		"Consider joining our Discord server at https://discord.gg/vcZTRPX",
+		"Consider joining our Discord server at https://discord.gg/KuUXcBuG5M",
 		"You can press sneak while jumping to jump up two blocks.",
-		"Use /donate <playername> <score> [message] (message optional) to reward a team-mate for their work.",
-		"A medic and knight working together can wreak havoc on the enemy team(s)",
-		"Use /lb to see a list of bountied players you can kill for score",
-		"In the Nade Fight mode, you can team up with someone using the void grenade for easier kills.",
-		"An alternative method to place markers is by (left/right) clicking while holding the zoom key (default: Z)",
-		"Use /mp <player> to send a marker to a specific teammate",
-		"Use /msg <playername> <message> to send a private message (PM) to another player.",
-		"To check someone's league in all modes, use /league <player>",
-		"Use /leagues to list leagues and the placement needed to get to them.",
-		"To place an HP marker, run /mhp or left/right click while holding the zoom key (default: Z) "..
-			"and looking at your feet",
-		"Use /map to print the current map name and its author.",
-		"To check someone's ranking in all modes, use /rank mode:all <player_name>",
-		"Building a base with materials stronger than cobblestone, such as wood or reinforced cobble, keeps your flag safer.",
+		"Use /donate <playername> <score> to reward a teammate for their work.",
+		"A medic and knight working together can wreak havoc on the enemy team(s).",
+		"Think someone is cheating? Send proofs to the email address loki(a)jma-sig.de",
+		"Join us on Discord: https://ctf.jma-sig.de",
+		"Use /news to see the server news and the Rules.",
+		"Please avoid or refrain from introducing any other potentially offensive or inappropriate topics.",
+		"Stuck? Use /kill to return to base",
+		"Strengthen your defense: build walls, set obstacles, and traps to secure your base.",
+		"Want to skip a match? Use /yes to vote during build time",
+		"How to access the Pro chest. Requirements: Score 8000, k/d 1.4 and 5 captured flags.",
+		-- connection, fps, minetest issues
+		"Make sure to use the latest version of Minetest (currently 5.7) for an optimal gaming experience.\nUsing outdated versions may lead to compatibility issues and lack of new features.",
+		"If you on older hardware or a mobile device, adjust to lower graphics for smoother gameplay, improving FPS.",
+		"Having a stable internet connection is crucial as it minimizes lag, ensuring smoother multiplayer interactions",
+		--translator usage tips
+		"Overcoming a language barrier? Use a translator in the chat by adding %<language code> after your message, e.g, %en.",
+		"Set your preferred language for in-game translator using /lang <lang code> . For example, use /lang en",
+		"Use /b to translate the last message to your preferred language",
 	}
 end
 
 function random_messages.display_message(message_number)
 	local msg = random_messages.messages[message_number] or message_number
 	if msg then
-		minetest.chat_send_all(minetest.colorize("#808080", S(msg)))
+		minetest.chat_send_all(minetest.colorize("#808080", msg))
 	end
 end
 
@@ -99,7 +93,7 @@ end
 function random_messages.list_messages()
 	local str = ""
 	for k,v in pairs(random_messages.messages) do
-		str = str .. k .. " | " .. S(v) .. "\n"
+		str = str .. k .. " | " .. v .. "\n"
 	end
 	return str
 end
@@ -123,14 +117,21 @@ function random_messages.save_messages()
 end
 
 --When server starts:
-random_messages.set_interval()
 random_messages.read_messages()
 
-local function step(dtime)
-	random_messages.show_message()
-	minetest.after(MESSAGE_INTERVAL, step)
-end
-minetest.after(MESSAGE_INTERVAL, step)
+local timer = 0
+minetest.register_globalstep(function(dtime)
+	timer = timer + dtime
+	if timer < MESSAGE_INTERVAL then
+		return
+	end
+
+	if #minetest.get_connected_players() > 0 then
+		random_messages.show_message()
+	end
+
+	timer = 0
+end)
 
 local register_chatcommand_table = {
 	params = "viewmessages | removemessage <number> | addmessage <number>",
