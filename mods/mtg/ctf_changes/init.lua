a70e301c2ff17796f1fd8f36c8b3203af297dcea
Nanowolf4 <n4w@tutanota.com>
2023-12-26 13:15:13 +0200

    ctf_changes: remove code of ABM's and falling_node param cleanup

--- mods/mtg/ctf_changes/init.lua
+++ mods/mtg/ctf_changes/init.lua
@@ -1,7 +1,5 @@
 local COOLDOWN = ctf_core.init_cooldowns()
 
-local DISALLOW_MOD_ABMS = {"default", "fire", "flowers", "tnt"}
-
 local node_fall_damage_factors = {
 	["default:snowblock"] = -14, -- From a height of 13 blocks you take 5 damage
 	["default:snow"] = -10, -- From a height of 13 blocks you take 6 damage
@@ -22,38 +20,6 @@ minetest.override_chatcommand("clearinv", {
 })
 
 minetest.register_on_mods_loaded(function()
-
-	-- Remove Unneeded ABMs
-
-	local remove_list = {}
-
-	for key, abm in pairs(minetest.registered_abms) do
-		for _, mod in pairs(DISALLOW_MOD_ABMS) do
-			if abm.mod_origin == mod then
-				table.insert(remove_list, key)
-				break
-			end
-		end
-	end
-
-	local removed = 0
-	for _, key in pairs(remove_list) do
-		table.remove(minetest.registered_abms, key - removed)
-		removed = removed + 1
-	end
-
-	-- Unset falling group for all nodes
-
-	for name, def in pairs(minetest.registered_nodes) do
-		if def.groups then
-			def.groups.falling_node = nil
-			minetest.override_item(name, {groups = def.groups})
-		end
-
-		if name:find("fire:") and def.on_timer then
-			def.on_timer = nil
-		end
-	end
 
 	-- Set item type and tiers for give_initial_stuff
 	local tiers = {"wood", "stone", "steel", "mese", "diamond"}
