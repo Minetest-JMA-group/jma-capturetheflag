423ea3f374ba8a708cf77e7069603627964c85de
Marko Petrović <petrovicmarko2006@gmail.com>
2023-12-29 21:30:57 +0100

    nameban: Add new mod

---
+++ mods/other/nameban/init.lua
@@ -0,0 +1,159 @@
+-- SPDX-License-Identifier: LGPL-2.1-only
+-- Copyright (c) 2023 Marko Petrović
+
+minetest.register_privilege("namelock")
+local storage = minetest.get_mod_storage()
+local db = minetest.deserialize(storage:get_string("database")) or {}
+
+local function make_logger(level)
+	return function(text, ...)
+		minetest.log(level, "[nameban] "..text:format(...))
+	end
+end
+
+local ACTION = make_logger("action")
+local WARNING = make_logger("warning")
+
+local function save_db()
+	storage:set_string("database", minetest.serialize(db))
+end
+
+local function findElement(data, string)
+	for i, word in ipairs(data) do
+		if word == string then
+			return i
+		end
+	end
+	return nil
+end
+
+local function patternExists(pattern, text)
+	local startPos, endPos = text:find(pattern)
+	return startPos ~= nil
+end
+
+local function parse_players(name)
+	db.namelock = db.namelock or {}
+	local whitelisted = findElement(db.namelock, name)
+	for _, word in ipairs(db) do
+		if patternExists(word, name) then
+			return "Your username is not allowed. Please, change it and relogin."
+		end
+	end
+	for _, word in ipairs(db.namelock) do
+		if patternExists(word, name) and not whitelisted then
+			return "Your username is not allowed. Please, change it and relogin."
+		end
+	end
+end
+
+minetest.register_chatcommand("wordban", {
+	description = "Add a word to the blacklist for what's allowed in player names",
+	params = "<word>",
+	privs = { ban=true },
+	func = function(name, params)
+		if params:match("[%p%s]") ~= nil then
+			return false, "You have to enter something that's possible to appear in the filename in the first place."
+		end
+		if findElement(db, params) then
+			return false, "Word "..params.." is already blacklisted."
+		end
+		table.insert(db, params)
+		save_db()
+
+		for _, player in ipairs(minetest.get_connected_players()) do
+			local playername = player:get_player_name()
+			local msg = parse_players(playername)
+			if msg then
+				minetest.kick_player(playername, msg)
+			end
+		end
+		if xban then
+			xban.report_to_discord("nameban: ***"..name.."*** has banned the word: "..params) 
+		end
+		return true, "Word "..params.." has been blacklisted."
+	end,
+})
+
+minetest.register_chatcommand("wordunban", {
+	description = "Remove a word from the blacklist for what's allowed in player names",
+	params = "<word>",
+	privs = { ban=true },
+	func = function(name, params)
+		local index = findElement(db, params)
+		if not index then
+			return false, "Word "..params.." doesn't exist in the blacklist."
+		end
+		table.remove(db, index)
+		save_db()
+		if xban then
+			xban.report_to_discord("nameban: ***"..name.."*** has UNbanned the word "..params) 
+		end
+		return true, "Word "..params.." has been removed from the blacklist."
+	end,
+})
+
+minetest.register_chatcommand("namelock", {
+	description = "Lock a name so that no other player with a similar name may log in",
+	params = "<playername>",
+	privs = { namelock=true },
+	func = function(name, params)
+		db.namelock = db.namelock or {}
+		if params:match("[%p%s]") ~= nil then
+			return false, "You have to enter something that's possible to appear in the filename in the first place."
+		end
+		if findElement(db.namelock, params) then
+			return false, "Name "..params.." is already locked"
+		end
+		table.insert(db.namelock, params)
+		save_db()
+
+		for _, player in ipairs(minetest.get_connected_players()) do
+			local playername = player:get_player_name()
+			local msg = parse_players(playername)
+			if msg then
+				minetest.kick_player(playername, msg)
+			end
+		end
+		if xban then
+			xban.report_to_discord("nameban: ***"..name.."*** has locked the playername "..params) 
+		end
+		return true, "Name "..params.." has been locked."
+	end,
+})
+
+minetest.register_chatcommand("nameunlock", {
+	description = "Unlock a name so that other players can use it as part of their username",
+	params = "<playername>",
+	privs = { namelock=true },
+	func = function(name, params)
+		db.namelock = db.namelock or {}
+		local index = findElement(db.namelock, params)
+		if not index then
+			return false, "Name "..params.." isn't locked."
+		end
+		table.remove(db.namelock, index)
+		save_db()
+		if xban then
+			xban.report_to_discord("nameban: ***"..name.."*** has UNlocked the playername "..params) 
+		end
+		return true, "Name "..params.." has been unlocked."
+	end,
+})
+
+minetest.register_on_prejoinplayer(parse_players)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
