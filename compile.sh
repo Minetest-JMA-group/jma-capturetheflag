5612ef1ba7edb6762d44126aaa64b030e2558fc3
Marko Petrovic <petrovicmarko2006@gmail.com>
2024-01-04 22:18:36 +0100

    Update compile script, enable compiler optimization

--- compile.sh
+++ compile.sh
@@ -9,5 +9,8 @@ if [ ! -d "$1" ]; then
     exit 1
 fi
 
-g++ "$1"/*.cpp -o "$1"/mylibrary.so -fPIC -llua5.3 -lQt5Core \
+g++ "$1"/*.cpp -o "$1"/mylibrary.so -fPIC -llua5.3 -lQt5Core -O2 \
 -I/usr/include/x86_64-linux-gnu/qt5 -I/usr/include/x86_64-linux-gnu/qt5/QtCore -shared -I/usr/include/lua5.3/
+
+patchelf --force-rpath "$1"/mylibrary.so
+patchelf --set-rpath /home/jmactf/server5.8/games/jma-capturetheflag/libs "$1"/mylibrary.so
