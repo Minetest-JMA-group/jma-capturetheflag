f4e841852901ecf1c92217fa8c3fe0a9cb5f1e9d
Nanowolf4 <n4w@tutanota.com>
2024-01-12 12:14:23 +0200

    flowers: remove ABMs

--- mods/mtg/mtg_flowers/init.lua
+++ mods/mtg/mtg_flowers/init.lua
@@ -116,70 +116,6 @@ for _,item in pairs(flowers.datas) do
 end
 
 
--- Flower spread
--- Public function to enable override by mods
-
-function flowers.flower_spread(pos, node)
-	pos.y = pos.y - 1
-	local under = minetest.get_node(pos)
-	pos.y = pos.y + 1
-	-- Replace flora with dry shrub in desert sand and silver sand,
-	-- as this is the only way to generate them.
-	-- However, preserve grasses in sand dune biomes.
-	if minetest.get_item_group(under.name, "sand") == 1 and
-			under.name ~= "default:sand" then
-		minetest.set_node(pos, {name = "default:dry_shrub"})
-		return
-	end
-
-	if minetest.get_item_group(under.name, "soil") == 0 then
-		return
-	end
-
-	local light = minetest.get_node_light(pos)
-	if not light or light < 13 then
-		return
-	end
-
-	local pos0 = vector.subtract(pos, 4)
-	local pos1 = vector.add(pos, 4)
-	-- Testing shows that a threshold of 3 results in an appropriate maximum
-	-- density of approximately 7 flora per 9x9 area.
-	if #minetest.find_nodes_in_area(pos0, pos1, "group:flora") > 3 then
-		return
-	end
-
-	local soils = minetest.find_nodes_in_area_under_air(
-		pos0, pos1, "group:soil")
-	local num_soils = #soils
-	if num_soils >= 1 then
-		for si = 1, math.min(3, num_soils) do
-			local soil = soils[math.random(num_soils)]
-			local soil_name = minetest.get_node(soil).name
-			local soil_above = {x = soil.x, y = soil.y + 1, z = soil.z}
-			light = minetest.get_node_light(soil_above)
-			if light and light >= 13 and
-					-- Only spread to same surface node
-					soil_name == under.name and
-					-- Desert sand is in the soil group
-					soil_name ~= "default:desert_sand" then
-				minetest.set_node(soil_above, {name = node.name})
-			end
-		end
-	end
-end
-
-minetest.register_abm({
-	label = "Flower spread",
-	nodenames = {"group:flora"},
-	interval = 13,
-	chance = 300,
-	action = function(...)
-		flowers.flower_spread(...)
-	end,
-})
-
-
 --
 -- Mushrooms
 --
@@ -220,40 +156,6 @@ minetest.register_node("flowers:mushroom_brown", {
 		type = "fixed",
 		fixed = {-3 / 16, -0.5, -3 / 16, 3 / 16, -2 / 16, 3 / 16},
 	}
-})
-
-
--- Mushroom spread and death
-
-function flowers.mushroom_spread(pos, node)
-	if minetest.get_node_light(pos, 0.5) > 3 then
-		if minetest.get_node_light(pos, nil) == 15 then
-			minetest.remove_node(pos)
-		end
-		return
-	end
-	local positions = minetest.find_nodes_in_area_under_air(
-		{x = pos.x - 1, y = pos.y - 2, z = pos.z - 1},
-		{x = pos.x + 1, y = pos.y + 1, z = pos.z + 1},
-		{"group:soil", "group:tree"})
-	if #positions == 0 then
-		return
-	end
-	local pos2 = positions[math.random(#positions)]
-	pos2.y = pos2.y + 1
-	if minetest.get_node_light(pos2, 0.5) <= 3 then
-		minetest.set_node(pos2, {name = node.name})
-	end
-end
-
-minetest.register_abm({
-	label = "Mushroom spread",
-	nodenames = {"group:mushroom"},
-	interval = 11,
-	chance = 150,
-	action = function(...)
-		flowers.mushroom_spread(...)
-	end,
 })
 
 
@@ -333,4 +235,3 @@ waterlily_waving_def.groups.not_in_creative_inventory = 1
 
 minetest.register_node("flowers:waterlily", waterlily_def)
 minetest.register_node("flowers:waterlily_waving", waterlily_waving_def)
-
