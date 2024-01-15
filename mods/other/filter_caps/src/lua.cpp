096e0ff550d2bfd4867e358825e5a84a35006690
Marko Petrović <petrovicmarko2006@gmail.com>
2024-01-15 14:57:45 +0100

    filter_caps: Add new mod

---
+++ mods/other/filter_caps/src/lua.cpp
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: LGPL-2.1-only
+// Copyright (c) 2023 Marko Petrović
+#include "lua.h"
+#include <QString>
+#include <QStringList>
+
+int parse(lua_State* L)
+{
+    if (!lua_isstring(L, 2)) {
+        lua_pushstring(L, "");
+        return 1;
+    }
+    QString text(lua_tostring(L, 2));
+    lua_Integer minLen = lua_tointeger(L, 1);
+
+    QString result;
+    for (QString &word : text.split(" ")) {
+        if (word.size() <= minLen) {
+            result += word + " ";
+            continue;
+        }
+        QChar first = word[0];
+        word = word.toLower();
+        word[0] = first;
+        result += word + " ";
+    }
+    lua_pushstring(L, result.toUtf8().data());
+    return 1;
+}
+
+extern "C" int luaopen_mylibrary(lua_State* L)
+{
+    lua_newtable(L);
+    lua_pushcfunction(L, parse);
+    lua_setfield(L, -2, "parse");
+
+    return 1;
+}
