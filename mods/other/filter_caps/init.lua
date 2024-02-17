8c332e51349bf8e4453e57bff1f1d4b3b4b3bd76
Marko Petrović <petrovicmarko2006@gmail.com>
2024-02-17 17:43:18 +0100

    filter_caps: Use algorithms for loading

--- mods/other/filter_caps/init.lua
+++ mods/other/filter_caps/init.lua
@@ -1,11 +1,1 @@
--- SPDX-License-Identifier: GPL-3.0-or-later
--- Copyright (c) 2023 Marko Petrović
-local MP = minetest.get_modpath(minetest.get_current_modname())
-local ie = minetest.request_insecure_environment()
-local libinit, err = ie.package.loadlib(MP.."/mylibrary.so", "luaopen_mylibrary")
-if not libinit and err then
-	minetest.log("[filter_caps]: Failed to load shared object file")
-	minetest.log("[filter_caps]: "..err)
-else
-	libinit()
-end
+algorithms.load_library()
