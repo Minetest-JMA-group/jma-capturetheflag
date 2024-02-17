e1f05b41ec8e4ca830bd39d16c925a7cb36b4588
Marko Petrović <petrovicmarko2006@gmail.com>
2024-02-17 19:01:27 +0100

    filter_caps: Add filter_caps.register_on_chat_message()

--- mods/other/filter_caps/init.lua
+++ mods/other/filter_caps/init.lua
@@ -1,1 +1,21 @@
+-- SPDX-License-Identifier: GPL-3.0-or-later
+-- Copyright (c) 2023 Marko Petrović
 algorithms.load_library()
+local registered_on_chat_message = {}
+
+filter_caps.register_on_chat_message = function(func)
+	table.insert(registered_on_chat_message, func)
+end
+
+minetest.register_on_chat_message(function(name, message)
+	if #registered_on_chat_message == 0 then
+		return false
+	end
+
+	message = filter_caps.parse(name, message)
+	for _, func in ipairs(registered_on_chat_message) do
+		if func(name, message) then
+			return true
+		end
+	end
+end)
