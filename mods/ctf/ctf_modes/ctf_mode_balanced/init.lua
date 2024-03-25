3426d3d96d030c47618c8e12b51d4af6ac4faa45
fancyfinn9 <142792026+fancyfinn9@users.noreply.github.com>
2024-03-25 17:38:32 +0000

    April fool's
    Add Rick Astley sword and Pokéball

---
+++ mods/ctf/ctf_modes/ctf_mode_balanced/init.lua
@@ -0,0 +1,86 @@
+local rankings = ctf_rankings.init()
+local recent_rankings = ctf_modebase.recent_rankings(rankings)
+local features = ctf_modebase.features(rankings, recent_rankings)
+
+local old_bounty_reward_func = ctf_modebase.bounties.bounty_reward_func
+local old_get_next_bounty = ctf_modebase.bounties.get_next_bounty
+ctf_modebase.register_mode("balanced", {
+	treasures = {
+		["default:ladder_wood" ] = {                max_count = 20, rarity = 0.3, max_stacks = 5},
+		["default:torch"       ] = {                max_count = 20, rarity = 0.3, max_stacks = 5},
+
+		["default:cobble"      ] = {min_count = 20, max_count = 99, rarity = 0.3, max_stacks = 2},
+		["default:wood"        ] = {min_count = 20, max_count = 99, rarity = 0.2, max_stacks = 2},
+
+		["ctf_teams:door_steel"] = {rarity = 0.2, max_stacks = 3},
+
+		["default:pick_steel"  ] = {rarity = 0.4, max_stacks = 3},
+		["default:shovel_steel"] = {rarity = 0.4, max_stacks = 2},
+		["default:axe_steel"   ] = {rarity = 0.4, max_stacks = 2},
+
+		["ctf_melee:sword_steel"  ] = {rarity = 0.2  , max_stacks = 2},
+
+		["ctf_ranged:pistol_loaded" ] = {rarity = 0.2 , max_stacks = 2},
+		["ctf_ranged:rifle_loaded"  ] = {rarity = 0.2                 },
+		["ctf_ranged:shotgun_loaded"] = {rarity = 0.05                },
+		["ctf_ranged:smg_loaded"    ] = {rarity = 0.05                },
+
+		["ctf_ranged:ammo" ] = {min_count = 3, max_count = 10, rarity = 0.3 , max_stacks = 2},
+		["default:apple"   ] = {min_count = 6, max_count = 16, rarity = 0.2 , max_stacks = 2},
+
+		["grenades:frag" ] = {rarity = 0.1, max_stacks = 1},
+		["grenades:smoke"] = {rarity = 0.2, max_stacks = 2},
+
+		["default:water_source"] = {rarity = 0.2, max_stacks = 1},
+
+		["ctf_mode_nade_fight:pokeball"] = {rarity = 0.4, max_stacks = 1},
+
+		["ctf_melee:sword_rick_astley"] = {rarity = 0.2, max_stacks = 2}
+	},
+	crafts = {"ctf_ranged:ammo", "ctf_melee:sword_steel", "ctf_melee:sword_mese", "ctf_melee:sword_diamond"},
+	physics = {sneak_glitch = true, new_move = false},
+	team_chest_items = {"default:cobble 99", "default:wood 99", "default:torch 30", "ctf_teams:door_steel 15"},
+	rankings = rankings,
+	recent_rankings = recent_rankings,
+	summary_ranks = {
+		_sort = "flag_attempts",
+		"score",
+		"flag_captures", "flag_attempts",
+		"kills", "kill_assists", "bounty_kills",
+		"deaths",
+		"hp_healed"
+	},
+
+	stuff_provider = function()
+		return {"default:sword_stone", "default:pick_stone", "default:torch 15", "default:stick 99"}
+	end,
+	initial_stuff_item_levels = features.initial_stuff_item_levels,
+	on_mode_start = function()
+		ctf_modebase.bounties.bounty_reward_func = ctf_modebase.bounty_algo.kd.bounty_reward_func
+		ctf_modebase.bounties.get_next_bounty = ctf_modebase.bounty_algo.kd.get_next_bounty
+	end,
+	on_mode_end = function()
+		ctf_modebase.bounties.bounty_reward_func = old_bounty_reward_func
+		ctf_modebase.bounties.get_next_bounty = old_get_next_bounty
+	end,
+	on_new_match = features.on_new_match,
+	on_match_end = features.on_match_end,
+	team_allocator = features.team_allocator,
+	on_allocplayer = features.on_allocplayer,
+	on_leaveplayer = features.on_leaveplayer,
+	on_dieplayer = features.on_dieplayer,
+	on_respawnplayer = features.on_respawnplayer,
+	can_take_flag = features.can_take_flag,
+	on_flag_take = features.on_flag_take,
+	on_flag_drop = features.on_flag_drop,
+	on_flag_capture = features.on_flag_capture,
+	on_flag_rightclick = function() end,
+	get_chest_access = features.get_chest_access,
+	player_is_pro = features.player_is_pro,
+	can_punchplayer = features.can_punchplayer,
+	on_punchplayer = features.on_punchplayer,
+	on_healplayer = features.on_healplayer,
+	calculate_knockback = function()
+		return 0
+	end,
+})
