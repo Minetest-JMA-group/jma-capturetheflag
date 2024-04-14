aec865839460db75857ae52b22d239e63a69c86b
Marko Petrović <petrovicmarko2006@gmail.com>
2024-04-14 16:51:20 +0200

    Switch architecture: aarch64

--- compile.sh
+++ compile.sh
@@ -4,11 +4,11 @@ if [ -z "$1" ]; then
 	exit 1
 fi
 if [ ! -d "$1" ]; then
-    echo "Argument is not a directory."
-    echo "Provide C++ (Qt) source project folder as argument"
-    exit 1
+	echo "Argument is not a directory."
+	echo "Provide C++ (Qt) source project folder as argument"
+	exit 1
 fi
 
 g++ "$1"/*.cpp -o "$1"/mylibrary.so -fPIC -lluajit-5.1 -lQt5Core -O2 \
--I/usr/include/x86_64-linux-gnu/qt5 -I/usr/include/x86_64-linux-gnu/qt5/QtCore -shared -I/usr/include/lua5.3/ -Ilibs/StorageSrc/ \
+-I/usr/include/aarch64-linux-gnu/qt5 -I/usr/include/aarch64-linux-gnu/qt5/QtCore -shared -I/usr/include/lua5.3/ -Ilibs/StorageSrc/ \
 -Wl,-rpath,/home/jmactf/server5.8/games/jma-capturetheflag/libs -Llibs/ $2
