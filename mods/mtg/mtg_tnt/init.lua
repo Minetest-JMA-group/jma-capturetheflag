5f7a398bc3fc094170ea025899ac3f61ca7d6454
Nanowolf4 <n4w@tutanota.com>
2024-08-16 08:27:54 +0300

    mtg_tnt: Make explode sound randomly pitched

--- mods/mtg/mtg_tnt/init.lua
+++ mods/mtg/mtg_tnt/init.lua
@@ -392,7 +392,7 @@ function tnt.boom(pos, def)
 		def.explode_center = true
 	end
 	local sound = def.sound or "tnt_explode"
-	minetest.sound_play(sound, {pos = pos, gain = 2.5,
+	minetest.sound_play(sound, {pos = pos, gain = 2.5, pitch = math.random(10, 20) / 10,
 			max_hear_distance = math.min(def.radius * 20, 128)}, true)
 	local drops, radius = tnt_explode(pos, def.radius, def.ignore_protection,
 			def.ignore_on_blast, def.ignore_indestructible, owner, def.explode_center)
