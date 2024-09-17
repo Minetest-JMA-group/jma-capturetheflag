e9764e525b041544dcb2e7ae48dea29dc65fd65c
Maintainer_ <131448485+FoxLoveFire@users.noreply.github.com>
2024-09-17 23:01:01 +0500

    Complete removal of a player from a team

--- mods/other/spectator/init.lua
+++ mods/other/spectator/init.lua
@@ -32,6 +32,7 @@ function spectator.on(player)
         spectator.spectators[name] = true
 
         ctf_teams.remove_online_player(player)
+        ctf_teams.player_team[name] = nil 
     end
 end
 
