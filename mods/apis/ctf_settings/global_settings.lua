5cd03cc6e31dd651d484a50a58683c61852517fe
Nanowolf4 <n4w@tutanota.com>
2024-09-29 11:55:36 +0300

    Fix use_hudbars setting and move to hudbars mod

--- mods/apis/ctf_settings/global_settings.lua
+++ mods/apis/ctf_settings/global_settings.lua
@@ -18,15 +18,3 @@ ctf_settings.register("ctf_kill_list:history_size", {
 		ctf_kill_list.apply_settings(player, true)
 	end
 })
-
-ctf_settings.register("use_hudbars", {
-	label = "Use modern hudbars instead of icon hud",
-	type = "bool",
-	default = "false",
-	description = "Use a bar with a label instead of icons for quantites like\n" ..
-		"health, stamina, and breath.\n" ..
-		"Reconnect to see effects.",
-	on_change = function(player, new_value)
-		minetest.chat_send_player(player:get_player_name(), "Please reconnect to apply setting: \"Use modern hudbars instead of icon hud\"")
-	end,
-})
