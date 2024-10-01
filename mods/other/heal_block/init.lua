4d9874683d3ad154ade2fabd9fc7d082d90b44a3
Nanowolf4 <n4w@tutanota.com>
2024-10-01 09:46:54 +0300

    heal_block: Various improvements and fixes

--- mods/other/heal_block/init.lua
+++ mods/other/heal_block/init.lua
@@ -1,50 +1,53 @@
 -- SPDX-License-Identifier: GPL-3.0-or-later
 -- Copyright (c) 2024 Ivan Shkatov (Maintainer_) ivanskatov672@gmail.com
-local last_heal_time = {}
+
 minetest.register_node("heal_block:heal", {
-    description = "Healing Block",
-    drawtype = "nodebox", 
-    node_box = { 
+    description = "Healing Block\n"
+        .. "A block that heals players within a 3-block radius.\n"
+        .. "Place it on your team's territory to keep your allies healthy nearby.\n"
+        .. minetest.colorize("yellow", "Warning: breaking this block will result in its loss, so defend it wisely!"),
+    drawtype = "nodebox",
+    node_box = {
         type = "fixed",
         fixed = {-0.5, -0.5, -0.5, 0.5, 0, 0.5}
     },
     walkable = true,
     tiles = {
-        {name = "heal_node.png", align_style = "repeat", scale = 1, position = {x = 0, y = 0, z = 0.5}}, -- top
-        {name = "default_snow.png", align_style = "repeat", scale = 1, position = {x = 0, y = 0, z = -0.5}}, -- bottom
-        {name = "default_snow.png", align_style = "repeat", scale = 1, position = {x = -0.5, y = 0, z = 0}}, -- left
-        {name = "default_snow.png", align_style = "repeat", scale = 1, position = {x = 0.5, y = 0, z = 0}}, -- right
-        {name = "default_snow.png", align_style = "repeat", scale = 1, position = {x = 0, y = -0.5, z = 0}}, -- front
-        {name = "default_snow.png", align_style = "repeat", scale = 1, position = {x = 0, y = 0.5, z = 0}} -- back
+        {name = "heal_block_top.png", align_style = "repeat", scale = 1, position = {x = 0, y = 0, z = 0.5}}, -- top
+        {name = "default_snow.png"}
     },
     groups = {snappy=2,cracky=3,oddly_breakable_by_hand=3},
     drop = "",
+
     on_place = function(itemstack, placer, pointed_thing)
         local pteam = ctf_teams.get(placer)
         if pteam then
             if not ctf_core.pos_inside(pointed_thing.under, ctf_teams.get_team_territory(pteam)) then
-                minetest.chat_send_player(placer:get_player_name(), "You can only place heal blocks in your own territory!")
-                return itemstack
+                hud_events.new(placer, {
+                    quick = true,
+                    text =  "Healing block can only be placed on your team's area.",
+                    color = "warning",
+                })
+                return
             end
         end
         minetest.item_place(itemstack, placer, pointed_thing)
         return itemstack
     end,
+
     on_construct = function(pos)
         minetest.get_node_timer(pos):start(1)
     end,
 
-    on_timer = function(pos, elapsed)
+    on_timer = function(pos)
         for _, player in ipairs(minetest.get_objects_inside_radius(pos, 3)) do
             if player:is_player() then
-                local player_name = player:get_player_name()
-                if not last_heal_time[player_name] or os.time() - last_heal_time[player_name] >= 1 then
-                    local hp = player:get_hp()
+                local hp = player:get_hp()
+                if hp < player:get_properties().hp_max then
                     player:set_hp(hp + 1)
-                    last_heal_time[player_name] = os.time()
                 end
             end
         end
-        return true 
+        return true
     end
 })
