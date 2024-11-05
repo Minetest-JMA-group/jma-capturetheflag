9068c2f87e56f675997a279160c11f7a0f699018
Nanowolf4 <n4w@tutanota.com>
2024-11-05 10:26:37 +0200

    ctf_mode_classes: Remove enderpearl item

--- mods/ctf/ctf_modes/ctf_mode_classes/init.lua
+++ mods/ctf/ctf_modes/ctf_mode_classes/init.lua
@@ -68,7 +68,6 @@ ctf_modebase.register_mode("classes", {
 		["default:water_source"] = {rarity = 0.2, max_stacks = 1},
 		["torch_bomb:torch_grenade"] = {rarity = 0.07, max_stacks = 1, min_count = 1, max_count = 4},
 		["easter_egg:egg"] = {rarity = 0.03, max_stacks = 1},
-		["enderpearl:ender_pearl"]  = {min_count = 1, max_count = 6, max_stacks = 1, rarity = 0.2},
 		["ctf_landmine:landmine"] = {min_count = 1, max_count =  8, max_stacks = 1, rarity = 0.2},
 
 	},
