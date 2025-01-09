1a04bd62bc50474ea5a68a8f9adcb27baae6f0a9
Nanowolf4 <n4w@tutanota.com>
2025-01-09 10:49:14 +0200

    Antispam: reduce code duplication, replace player ban with temporary mute

--- mods/other/antispam/init.lua
+++ mods/other/antispam/init.lua
@@ -10,16 +10,41 @@ local SPAM_SPEED = 5
 local SPAM_SPEED_MSECS = SPAM_SPEED * 1e6
 local SPAM_WARN = 3
 local SPAM_KICK = SPAM_WARN + 4
-local SPAM_BAN_AFTER_KICKS = 2
+local SPAM_MUTE_AFTER_KICKS = 2
 local RESET_TIME = 30
-local RESET_TIME_MSECS = RESET_TIME * 1e6
 local WARNING_COLOR = minetest.get_color_escape_sequence("#FFBB33")
+local MUTE_TIME = 60*10
 
 local function init_player_state(player_name)
     if not PLAYER_KICKS[player_name] then PLAYER_KICKS[player_name] = 0 end
     if not PLAYERS_MSG[player_name] then PLAYERS_MSG[player_name] = {} end
-    if not PLAYERS_FREQ[player_name] then 
-        PLAYERS_FREQ[player_name] = {0, 0, minetest.get_us_time(), 0} 
+    if not PLAYERS_FREQ[player_name] then
+        PLAYERS_FREQ[player_name] = {0, 0, minetest.get_us_time(), 0}
+    end
+end
+
+local function increment_spam_counter(name)
+    PLAYER_KICKS[name] = PLAYER_KICKS[name] + 1
+    if PLAYER_KICKS[name] >= SPAM_MUTE_AFTER_KICKS then
+        local expires = os.time() + MUTE_TIME
+        local ok, e = xban.mute_player(name, "Antispam", expires, "Muted for spamming")
+
+        if ok then
+            minetest.log("action", string.format("[Antispam] Player %s muted for %d minutes due to spam", name, MUTE_TIME/60))
+        else
+            minetest.log("action", string.format("[Antispam] Failed to mute player %s: %s", name, e))
+        end
+
+        -- Reset player data after get muted
+        PLAYER_KICKS[name] = nil
+        PLAYERS_MSG[name] = nil
+        PLAYERS_FREQ[name] = nil
+        init_player_state(name)
+        return true
+    else
+        minetest.kick_player(name, "Kicked for spamming.")
+        minetest.log("action", "[Antispam] Player " .. name .. " kicked for spamming.")
+        return false
     end
 end
 
@@ -32,18 +57,8 @@ local function handle_message(name, message)
         message_info[2] = current_time
 
         if message_info[1] >= SPAM_KICK then
-            PLAYER_KICKS[name] = PLAYER_KICKS[name] + 1
-            if PLAYER_KICKS[name] >= SPAM_BAN_AFTER_KICKS then
-                minetest.log("[Antispam] Player " .. name .. " Banned for spamming.")
-                minetest.ban_player(name, "Banned for spamming.")
-                -- Clear player data after ban
-                PLAYER_KICKS[name] = nil
-                PLAYERS_MSG[name] = nil
-                PLAYERS_FREQ[name] = nil
-                return true  -- Player is banned, stop further processing
-            else
-                minetest.kick_player(name, "Kicked for spamming.")
-                minetest.log("[Antispam] Player " .. name .. " kicked for spamming.")
+            if increment_spam_counter(name) then
+                return true  -- Player is muted, stop further processing
             end
         elseif message_info[1] >= SPAM_WARN then
             minetest.chat_send_player(name, WARNING_COLOR .. "Warning! You've sent the message '" .. message .. "' too often. Wait at least " .. RESET_TIME .. " seconds before sending it again.")
@@ -54,21 +69,11 @@ local function handle_message(name, message)
 
     local player_freq = PLAYERS_FREQ[name]
     local speed = (player_freq[1] * player_freq[2] + (current_time - player_freq[3])) / (player_freq[2] + 1)
-    
+
     if player_freq[2] >= SPAM_WARN then
         if player_freq[4] + 1 >= SPAM_KICK - SPAM_WARN then
-            PLAYER_KICKS[name] = PLAYER_KICKS[name] + 1
-            if PLAYER_KICKS[name] >= SPAM_BAN_AFTER_KICKS then
-                minetest.log("[Antispam] Player " .. name .. " Banned for spamming.")
-                minetest.ban_player(name, "Banned for spamming.")
-                -- Clear player data after ban
-                PLAYER_KICKS[name] = nil
-                PLAYERS_MSG[name] = nil
-                PLAYERS_FREQ[name] = nil
-                return true 
-            else
-                minetest.kick_player(name, "Kicked for spamming.")
-                 minetest.log("[Antispam] Player " .. name .. " kicked for spamming.")
+            if increment_spam_counter(name) then
+                return true
             end
         elseif speed <= SPAM_SPEED_MSECS then
             minetest.chat_send_player(name, WARNING_COLOR .. "Warning! You're sending messages too fast. Wait at least " .. SPAM_SPEED .. " seconds.")
@@ -89,7 +94,7 @@ end
 
 minetest.register_chatcommand("antispam", {
     params = "<option> <value>",
-    description = "Change spam settings: <option> can be 'speed', 'warn', 'kick', or 'ban_kicks'. <value> is the new value for that option.",
+    description = "Change spam settings: <option> can be 'speed', 'warn', 'kick', or 'mute_threshold'. <value> is the new value for that option.",
     privs = {server = true},
     func = function(name, param)
         local argv = param:split(" ")
@@ -110,10 +115,10 @@ minetest.register_chatcommand("antispam", {
         elseif option == "warn" then
             SPAM_WARN = value
             SPAM_KICK = SPAM_WARN + 4
-        elseif option == "ban_kicks" then
-            SPAM_BAN_AFTER_KICKS = value
+        elseif option == "mute_threshold" then
+            SPAM_MUTE_AFTER_KICKS = value
         else
-            return false, "Invalid option. Available options: speed, warn, kick, ban_kicks."
+            return false, "Invalid option. Available options: speed, warn, kick, mute_threshold."
         end
 
         return true, minetest.colorize("#FF0000", "[Antispam] ") .. minetest.colorize("#00FF00", "Option " .. option .. " set to " .. value)
