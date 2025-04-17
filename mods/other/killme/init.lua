14e3a88c12204608b6d9c6208a606e7b6c8a4090
Nanowolf4 <n4w@tutanota.com>
2025-04-17 08:25:59 +0300

    Move /kickme and /killme to essentials

--- mods/other/killme/init.lua
+++
@@ -1,9 +0,0 @@
-minetest.register_chatcommand("killme", {
-	description = "Kill yourself to respawn",
-	func = function(name, _)
-		local player = minetest.get_player_by_name(name)
-		if player then
-			player:set_hp(0, name.." executed killme.")
-		end
-	end
-})
