7dc35b01040f6d1d377a0853856569b748cc4a4b
Nanowolf4 <n4w@tutanota.com>
2025-05-08 10:30:26 +0300

    dropondie: Assing "dropped_by" in the item's metadata

--- mods/pvp/dropondie/init.lua
+++ mods/pvp/dropondie/init.lua
@@ -1,7 +1,18 @@
 dropondie = {}
 
-local function drop_list(pos, vel, inv, list)
+local function drop_list(pos, player, list)
+	local vel = player:get_velocity()
+	local pname = player:get_player_name()
+	local is_player = pname ~= ""
+	local inv = player:get_inventory()
 	for _, item in ipairs(inv:get_list(list)) do
+		if minetest.registered_items[item:get_name()].stack_max == 1 then
+			if is_player then
+				local meta = item:get_meta()
+				meta:set_string("dropped_by", pname)
+			end
+		end
+
 		local obj = minetest.add_item(pos, item)
 		if obj then
 			local random_velocity = {
@@ -28,7 +39,7 @@ function dropondie.drop_all(player)
 	local pos = player:get_pos()
 	pos.y = math.floor(pos.y + 0.5)
 
-	drop_list(pos, player:get_velocity(), player:get_inventory(), "main")
+	drop_list(pos, player, "main")
 end
 
 if ctf_core.settings.server_mode ~= "mapedit" then
