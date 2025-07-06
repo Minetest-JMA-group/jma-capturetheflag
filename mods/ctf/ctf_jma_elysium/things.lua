ac565989db1c98a362a0e2940b605dfaef1c4953
Nanowolf4 <n4w@tutanota.com>
2025-07-06 11:25:29 +0300

    ctf_jma_elysium: Add Infinite Apple

--- mods/ctf/ctf_jma_elysium/things.lua
+++ mods/ctf/ctf_jma_elysium/things.lua
@@ -1,3 +1,5 @@
+local COOLDOWN = ctf_core.init_cooldowns()
+
 local pvp_switch_def = {
 	description = "Switch pvp mode (Currently PVP ON)",
 	inventory_image = "ctf_jma_elysium_pvp_on.png",
@@ -35,9 +37,32 @@ pvp_switch_def.description = "Switch pvp mode (Currently PVP OFF)"
 pvp_switch_def.inventory_image = "ctf_jma_elysium_pvp_off.png"
 core.register_craftitem("ctf_jma_elysium:pvp_off", pvp_switch_def)
 
+local apple_def = table.copy(core.registered_nodes["default:apple"])
+apple_def.description = "Infinite Apple (Copy of default apple)"
+apple_def.tiles = {"ctf_jma_elysium_inf_apple.png"}
+apple_def.inventory_image = "ctf_jma_elysium_inf_apple.png"
+apple_def.stack_max = 1
+apple_def.on_use = function(itemstack, user, ...)
+	if not COOLDOWN:get(user) then
+		COOLDOWN:set(user, 0.2)
+
+		local hp = user:get_hp()
+		if hp > 0 then
+			user:set_hp(hp + 3)
+		end
+	end
+end
+core.register_node("ctf_jma_elysium:inf_apple", apple_def)
+
+local deny_items = {
+	["ctf_jma_elysium:pvp_on"] = true,
+	["ctf_jma_elysium:pvp_off"] = true
+}
+
 core.register_allow_player_inventory_action(function(player, action, inventory, info)
-	if ctf_jma_elysium.players[player:get_player_name()] then
-		if action == "take" and info.stack:get_name():match("^ctf_jma_elysium") then
+	if ctf_jma_elysium.get_player(player:get_player_name()) then
+		local itemname = info.stack:get_name()
+		if action == "take" and deny_items[itemname] then
 			return 0
 		end
 	end
