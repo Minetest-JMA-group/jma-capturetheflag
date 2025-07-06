b0596366b4578ba9bb5589f1dcf7c6c7fbbe0e06
Nanowolf4 <n4w@tutanota.com>
2025-07-06 15:48:05 +0300

    wind_charges: Wind charge now affects only players and __builtin:item

--- mods/other/wind_charges/init.lua
+++ mods/other/wind_charges/init.lua
@@ -1,7 +1,7 @@
 wind_charges = {}
 
 local function is_solid_node_near(pos)
-    local radius = 0.1
+	local radius = 0.1
 	for _, offset in ipairs({
 		{x = radius, y = 0, z = 0}, {x = -radius, y = 0, z = 0},
 		{x = 0, y = radius, z = 0}, {x = 0, y = -radius, z = 0},
@@ -23,84 +23,81 @@ minetest.register_entity("wind_charges:wind_projectile", {
 	textures = {"wind_charge.png"},
 	visual_size = {x = 0.5, y = 0.5},
 	timer = 0,
-	lifetime = 16,
+	lifetime = 1,
 	explode = function(self)
 		local pos = self.object:get_pos()
+
 		minetest.sound_play("wind_charge_explode", {
 			pos = pos,
-			max_hear_distance = 8,
+			max_hear_distance = 5,
 			gain = 1.0
 		})
 
-        minetest.add_particlespawner({
-            amount = 8,
-            time = 0.1,
-            minpos = pos,
-            maxpos = pos,
-            minvel = {x = -5, y = 0, z = -5},
-            maxvel = {x = 5, y = 2, z = 5},
-            minacc = {x = 0, y = 0, z = 0},
-            maxacc = {x = 0, y = -5, z = 0},
-            minexptime = 0.1,
-            maxexptime = 0.2,
-            minsize = 5,
-            maxsize = 8,
-            texture = "wind_charge_explode_particle.png",
-            glow = 5,
-        })
+		minetest.add_particlespawner({
+			amount = 8,
+			time = 0.1,
+			minpos = pos,
+			maxpos = pos,
+			minvel = {x = -5, y = 0, z = -5},
+			maxvel = {x = 5, y = 2, z = 5},
+			minacc = {x = 0, y = 0, z = 0},
+			maxacc = {x = 0, y = -5, z = 0},
+			minexptime = 0.1,
+			maxexptime = 0.2,
+			minsize = 5,
+			maxsize = 8,
+			texture = "wind_charge_explode_particle.png",
+			glow = 5,
+		})
 
 		local objs = minetest.get_objects_inside_radius(pos, 4)
-		for obj_index, obj in ipairs(objs) do
-			if obj:is_player() then
-				local player = obj
-				local player_name = player:get_player_name()
-				if ctf_teams.get(self.owner_name) == ctf_teams.get(player_name) and self.owner_name ~= player_name then--check if player_name is in the same team as the owner
-					goto continue
-				end
+		for _, obj in ipairs(objs) do
+			local ent = obj:get_luaentity()
+
+			if not obj:is_player() and (not ent or ent.name ~= "__builtin:item") then
+				goto continue
 			end
+
 			local obj_pos = obj:get_pos()
 			local dist = vector.distance(pos, obj_pos)
 			local dir = vector.direction(pos, obj_pos)
 			local power = 16 * (1 - math.min(dist / 4, 1))
+
 			if obj:is_player() then
 				dir.y = 1
 			else
-				local ent = obj:get_luaentity()
-				if ent and ent.name == "__builtin:item" then
-					local owner_name = self.owner_name
-					local owner = minetest.get_player_by_name(owner_name)
-					if not owner then
-						goto continue
-					end
+				local owner_name = self.owner_name
+				local owner = minetest.get_player_by_name(owner_name)
+				if owner then
 					local owner_pos = owner:get_pos()
 					dir = vector.direction(pos, owner_pos)
 					power = dist * 3 + 2
-
 					obj:add_velocity({x = 0, y = 4, z = 0})
 					dir.y = 0
 				else
-					dir.y = 0.4
+					goto continue
 				end
 			end
 			obj:add_velocity(vector.multiply(dir, power))
-		    ::continue::
+			::continue::
 		end
+
 		self.object:remove()
 	end,
 
-    on_step = function(self, dtime)
-        self.timer = self.timer + dtime
-        if self.timer > self.lifetime then
-            self:explode()
-            return
-        end
+	on_step = function(self, dtime)
+		self.timer = self.timer + dtime
+		if self.timer > self.lifetime then
+			self:explode()
+			return
+		end
 
-        local pos = self.object:get_pos()
-        if is_solid_node_near(pos) then
-            self:explode()
-            return
-        end
-    end,
+		local pos = self.object:get_pos()
+		if is_solid_node_near(pos) then
+			self:explode()
+			return
+		end
+	end,
 	on_activate = function(self, staticdata, dtime_s)
 		if staticdata and staticdata:match("^owner:") then
 			self.owner_name = staticdata:sub(7)
@@ -143,7 +140,7 @@ minetest.register_craftitem("wind_charges:wind_charge", {
 
 			minetest.sound_play("wind_charge_throw", {
 				pos = pos,
-				max_hear_distance = 8,
+				max_hear_distance = 5,
 				gain = 0.8
 			})
 		end
