db5988fc84582064aa193457a9d10b44aeb6294c
A-0234 <eddypeng0234@gmail.com>
2025-09-24 21:42:57 -0500

    Update init.lua of rocket_launcher

--- mods/other/rocket_launcher/init.lua
+++ mods/other/rocket_launcher/init.lua
@@ -1,5 +1,6 @@
 local default_radius = 3
-
+local rocket_cooldowns = {}
+local COOLDOWN_TIME = 4 
 
 minetest.register_craftitem("rocket_launcher:rocket", {
 	wield_scale = {x=2,y=2,z=1.5},
@@ -8,17 +9,18 @@ minetest.register_craftitem("rocket_launcher:rocket", {
 	inventory_image = "rocket.png",
 })
 
-local WEAR_MAX = 65535
 minetest.register_tool("rocket_launcher:launcher", {
 	wield_scale = {x=2,y=2,z=2},
 	description = "Rocket Launcher\nUses rockets",
 	inventory_image = "rocket_launcher.png",
 	on_use = function(itemstack, user)
-		if itemstack:get_wear() > 0 then
-			return
-		end
 		local name = user:get_player_name()
 		local inv = user:get_inventory()
+		local last_fire = rocket_cooldowns[name] or 0
+		if os.time() - last_fire < COOLDOWN_TIME then
+			minetest.chat_send_player(name, "Rocket Launcher is on cooldown!")
+			return itemstack
+		end
 		if inv:contains_item("main", "rocket_launcher:rocket") then
 			inv:remove_item("main", "rocket_launcher:rocket 1")
 			local pos = user:get_pos()
@@ -39,10 +41,11 @@ minetest.register_tool("rocket_launcher:launcher", {
 				end
 			end
 			minetest.sound_play('fire_extinguish_flame',{to_player = name, gain = 0.5})
-			itemstack:set_wear(WEAR_MAX - 6000)
-			ctf_modebase.update_wear.start_update(user:get_player_name(), itemstack, WEAR_MAX/4, true)
-			return itemstack
+			rocket_cooldowns[name] = os.time()
+		else
+			minetest.chat_send_player(name, "You have no rockets!")
 		end
+		return itemstack
 	end
 })
 
@@ -68,9 +71,7 @@ local function can_explode(pos, pname, radius)
 		minetest.chat_send_player(pname, "You can't explode rocket on spawn")
 		return false
 	end
-
 	local pteam = ctf_teams.get(pname)
-
 	if pteam then
 		for flagteam, team in pairs(ctf_map.current_map.teams) do
 			if not ctf_modebase.flag_captured[flagteam] and team.flag_pos then
@@ -89,50 +90,47 @@ rocket.on_step = function(self, dtime, moveresult)
 	self.timer = self.timer + dtime
 	local pos = self.object:get_pos()
 	if not pos then return end
-	minetest.after(0.1,function()
-		minetest.add_particle({
-			pos = pos,
-			velocity = {x=math.random(-1,1),y=math.random(-1,1),z=math.random(-1,1)},
-			expirationtime = 1.9,
-			size = 6,
-			collisiondetection = false,
-			vertical = false,
-			texture = "tnt_smoke.png",
-			glow = 15})
-	end)
 
-	if self.timer >= 60 then
-		self.object:remove()
-	end
+	minetest.add_particle({
+		pos = pos,
+		velocity = {x=math.random(-1,1),y=math.random(-1,1),z=math.random(-1,1)},
+		expirationtime = 1.9,
+		size = 6,
+		collisiondetection = false,
+		vertical = false,
+		texture = "tnt_smoke.png",
+		glow = 15})
+
+	if self.timer >= 60 then self.object:remove() end
+
 	if self.timer > 0.2 then
-		local objs = minetest.get_objects_inside_radius({x = pos.x, y = pos.y-1, z = pos.z}, 1.3)
-		for k, obj in pairs(objs) do
+		local objs = minetest.get_objects_inside_radius({x=pos.x, y=pos.y-1, z=pos.z}, 1.3)
+		for _, obj in pairs(objs) do
 			local prop = obj and obj:get_properties()
-			if prop then
-				if obj:is_player() or prop.collide_with_objects == true then
-					if can_explode(pos, self.puncher_name, self.radius) then
-						tnt.boom(pos, {
-							radius = self.radius,
-							puncher_name = self.puncher_name
-						})
-					end
-					self.object:remove()
+			if prop and (obj:is_player() or prop.collide_with_objects == true) then
+				if can_explode(pos, self.puncher_name, self.radius) then
+					tnt.boom(pos, {radius = self.radius, puncher_name = self.puncher_name})
 				end
+				self.object:remove()
 			end
 		end
 	end
 
 	if moveresult.collides then
-		if can_explode(pos, self.puncher_name, self.radius)  then
-			tnt.boom(pos, {
-				radius = self.radius,
-				-- ignore_indestructible = true,
-				puncher_name = self.puncher_name
-			})
+		if can_explode(pos, self.puncher_name, self.radius) then
+			tnt.boom(pos, {radius = self.radius, puncher_name = self.puncher_name})
 		end
 		self.object:remove()
 	end
 end
 
-
 minetest.register_entity("rocket_launcher:rocket", rocket)
+
+minetest.register_on_respawnplayer(function(player)
+	rocket_cooldowns[player:get_player_name()] = 0
+end)
+
+minetest.register_on_leaveplayer(function(player)
+	rocket_cooldowns[player:get_player_name()] = nil
+end)
+
