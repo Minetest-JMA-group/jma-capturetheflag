499ec9a9e03c0cf15d3b4c3b44350801533a6a20
Farooq Karimi Zadeh <fkz@riseup.net>
2025-09-25 12:22:29 +0330

    make ctf_jma_elysium code pretty and use translation API for it

--- mods/ctf/ctf_jma_elysium/map_utils.lua
+++ mods/ctf/ctf_jma_elysium/map_utils.lua
@@ -1,5 +1,6 @@
 local storage = core.get_mod_storage()
 
+local S = core.get_translator(core.get_current_modname())
 local function serialize_inventory(t)
 	local result = {}
 	for list_name, stacks in pairs(t) do
@@ -25,7 +26,10 @@ end
 function ctf_jma_elysium.restore_nodemeta(mapname)
 	local t = core.deserialize(storage:get_string("meta_" .. mapname))
 	if not t then
-		core.log("warning", "Failed to restore metadata for " .. mapname .. ": no metadata saved")
+		core.log(
+			"warning",
+			"Failed to restore metadata for " .. mapname .. ": no metadata saved"
+		)
 		return false
 	end
 
@@ -44,11 +48,11 @@ function ctf_jma_elysium.restore_nodemeta(mapname)
 		local meta = core.get_meta(saved_pos)
 
 		if saved.meta_fields then
-			meta:from_table({fields = saved.meta_fields})
+			meta:from_table({ fields = saved.meta_fields })
 		end
 
 		if saved.inventory then
-			local inv = core.get_inventory({type="node", pos = saved_pos})
+			local inv = core.get_inventory({ type = "node", pos = saved_pos })
 			if inv then
 				local res = deserialize_inventory(saved.inventory)
 				inv:set_lists(res)
@@ -62,7 +66,7 @@ function ctf_jma_elysium.restore_nodemeta(mapname)
 
 		core.log("action", "Restored node meta at: " .. core.pos_to_string(saved_pos))
 		-- else
-			-- core.log("warning", "Expected node '" .. (saved.node_name or "?") .. "' not found at: " .. core.pos_to_string(saved_pos))
+		-- core.log("warning", "Expected node '" .. (saved.node_name or "?") .. "' not found at: " .. core.pos_to_string(saved_pos))
 		-- end
 	end
 
@@ -90,16 +94,18 @@ function ctf_jma_elysium.save_meta(mapname)
 	local vm = VoxelManip()
 	local emerged_min, emerged_max = vm:read_from_map(minp, maxp)
 	local data = vm:get_data()
-	local area = VoxelArea:new{MinEdge = emerged_min, MaxEdge = emerged_max}
+	local area = VoxelArea:new({ MinEdge = emerged_min, MaxEdge = emerged_max })
 
 	local count = 0
 	local to_save = {}
 
 	for index in area:iterp(minp, maxp) do
 		local node_id = data[index]
-		if node_id ~= core.CONTENT_IGNORE and node_id ~= core.CONTENT_AIR and
-		node_id ~= core.CONTENT_UNKNOWN then
-
+		if
+			node_id ~= core.CONTENT_IGNORE
+			and node_id ~= core.CONTENT_AIR
+			and node_id ~= core.CONTENT_UNKNOWN
+		then
 			local cpos = area:position(index)
 			local meta = core.get_meta(cpos):to_table()
 			local node = core.get_node(cpos)
@@ -152,7 +158,7 @@ function ctf_jma_elysium.replace_nodes_with_ctf_map(mapname)
 	local vm = VoxelManip()
 	local emerged_min, emerged_max = vm:read_from_map(minp, maxp)
 	local data = vm:get_data()
-	local area = VoxelArea:new{MinEdge = emerged_min, MaxEdge = emerged_max}
+	local area = VoxelArea:new({ MinEdge = emerged_min, MaxEdge = emerged_max })
 
 	local replaced_count = 0
 	local skipped_count = 0
@@ -160,9 +166,11 @@ function ctf_jma_elysium.replace_nodes_with_ctf_map(mapname)
 	for index in area:iterp(minp, maxp) do
 		local node_id = data[index]
 
-		if node_id ~= core.CONTENT_IGNORE and node_id ~= core.CONTENT_AIR and
-		   node_id ~= core.CONTENT_UNKNOWN then
-
+		if
+			node_id ~= core.CONTENT_IGNORE
+			and node_id ~= core.CONTENT_AIR
+			and node_id ~= core.CONTENT_UNKNOWN
+		then
 			local node_name = core.get_name_from_content_id(node_id)
 
 			if not string.match(node_name, "^ctf_map:") then
@@ -185,32 +193,36 @@ function ctf_jma_elysium.replace_nodes_with_ctf_map(mapname)
 end
 
 core.register_chatcommand("el_save_meta", {
-	description = "Save metadata in the current map region",
-	privs = {server = true},
+	description = S("Save metadata in the current map region"),
+	privs = { server = true },
 	func = function(name, mapname)
 		if not ctf_jma_elysium.loaded_maps[mapname] then
-			return false, "The map does not exist or not loaded"
+			return false, S("The map does not exist or not loaded")
 		end
 
 		local result, count = ctf_jma_elysium.save_meta(mapname)
 		if result then
-			return true, "The nodes metadata has been saved to modstorage. Nodes count: " .. count
+			return true,
+				S(
+					"The nodes metadata has been saved to modstorage. Nodes count: @1",
+					count
+				)
 		end
-		return false, "Failed to save"
-	end
+		return false, S("Failed to save")
+	end,
 })
 
 core.register_chatcommand("el_restore_meta", {
-	description = "Restore node metadata",
-	privs = {server = true},
+	description = S("Restore node metadata"),
+	privs = { server = true },
 	func = function(namem, mapname)
 		if not ctf_jma_elysium.loaded_maps[mapname] then
-			return false, "The map does not exist or not loaded"
+			return false, S("The map does not exist or not loaded")
 		end
 
 		ctf_jma_elysium.restore_nodemeta(mapname)
-		return true, "Restored"
-	end
+		return true, S("Restored")
+	end,
 })
 
 local export_path = core.get_worldpath() .. "/maps_meta/"
@@ -218,8 +230,8 @@ minetest.mkdir(export_path) -- make sure the directory exists
 
 core.register_chatcommand("el_meta", {
 	params = "export|import <mapname>",
-	description = "Export or import metadata of the map",
-	privs = {server = true},
+	description = S("Export or import metadata of the map"),
+	privs = { server = true },
 	func = function(name, param)
 		local args = string.split(param, " ", false, 2)
 		local action = args[1]
@@ -234,74 +246,89 @@ core.register_chatcommand("el_meta", {
 		if action == "export" then
 			local data = storage:get_string("meta_" .. mapname)
 			if data == "" then
-				return false, "No metadata saved, nothing to export"
+				return false, S("No metadata saved, nothing to export")
 			end
 
 			local f = io.open(path, "w")
-			if not f then return false, "Cannot open file for writing: " .. path end
+			if not f then
+				return false, S("Cannot open file for writing: @1", path)
+			end
 			f:write(data)
 			f:close()
-			return true, "Exported metadata to " .. path
-
+			return true, S("Exported metadata to @1", path)
 		elseif action == "import" then
 			local f = io.open(path, "r")
-			if not f then return false, "File not found: " .. path end
+			if not f then
+				return false, S("File not found: ", path)
+			end
 
 			local contents = f:read("*a")
 			f:close()
 
 			if type(core.deserialize(contents)) ~= "table" then
-				return false, "Invalid metadata format"
+				return false, S("Invalid metadata format")
 			end
 
 			storage:set_string("meta_" .. mapname, contents)
 			ctf_jma_elysium.restore_nodemeta(mapname)
-			return true, "Imported metadata from " .. path
-
+			return true, S("Imported metadata from ", path)
 		else
-			return false, "Unknown action. Available actions: import, export"
+			return false, S("Unknown action. Available actions:") .. "import, export"
 		end
-	end
+	end,
 })
 
 core.register_chatcommand("el_get_area", {
 	params = "<mapname>",
-	description = "Show local coordinates of your WorldEdit selection for the given map",
-	privs = {server = true},
+	description = S(
+		"Show local coordinates of your WorldEdit selection for the given map"
+	),
+	privs = { server = true },
 	func = function(name, mapname)
 		if not mapname or mapname == "" then
-			return false, "Invalid map name"
+			return false, S("Invalid map name")
 		end
 		local mapdef = ctf_jma_elysium.maps[mapname]
 		if not mapdef then
-			return false, "Map not found: " .. mapname
+			return false, S("Map not found: ", mapname)
 		end
 		local we = worldedit.pos1[name] and worldedit.pos2[name] and worldedit
 		if not we then
-			return false, "You must select a region with WorldEdit first."
+			return false, S("You must select a region with WorldEdit first.")
 		end
 		local pos1 = worldedit.pos1[name]
 		local pos2 = worldedit.pos2[name]
 
 		local local_min = vector.subtract(pos1, mapdef.pos1)
 		local local_max = vector.subtract(pos2, mapdef.pos1)
-		return true, string.format(
-			"Local region: min (%d, %d, %d), max (%d, %d, %d)",
-			local_min.x, local_min.y, local_min.z,
-			local_max.x, local_max.y, local_max.z
-		)
-	end
+		return true,
+			S(
+				"Local region: min (@1, @2, @3), max (@4, @5, @6)",
+				local_min.x,
+				local_min.y,
+				local_min.z,
+				local_max.x,
+				local_max.y,
+				local_max.z
+			)
+	end,
 })
 
 core.register_chatcommand("el_replace_nodes", {
-	description = "Replace all nodes in the current map region with ctf_map variants",
-	privs = {server = true},
+	description = S("Replace all nodes in the current map region with ctf_map variants"),
+	privs = { server = true },
 	func = function(name, mapname)
 		if not ctf_jma_elysium.loaded_maps[mapname] then
-			return false, "The map does not exist or not loaded"
+			return false, S("The map does not exist or not loaded")
 		end
 
-		local replaced_count, skipped_count = ctf_jma_elysium.replace_nodes_with_ctf_map(mapname)
-		return true, "Nodes replacement completed. Replaced: " .. replaced_count .. ", Skipped: " .. skipped_count
-	end
+		local replaced_count, skipped_count =
+			ctf_jma_elysium.replace_nodes_with_ctf_map(mapname)
+		return true,
+			S(
+				"Nodes replacement completed. Replaced: @1, Skipped: @2",
+				replaced_count,
+				skipped_count
+			)
+	end,
 })
