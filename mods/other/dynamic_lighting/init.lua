3dddc6fbc7985d0d2264a9187575e2b51730c27b
Farooq Karimi Zadeh <fkz@riseup.net>
2025-09-26 14:22:31 +0330

    make code of dynamic_lighting mod pretty

--- mods/other/dynamic_lighting/init.lua
+++ mods/other/dynamic_lighting/init.lua
@@ -7,11 +7,14 @@ wielded_light = {}
 
 function wielded_light.update_light(pos, light_level)
 	local around_vector = {
-			{x=0, y=0, z=0},
-			{x=0, y=1, z=0}, {x=0, y=-1, z=0},
-			{x=1, y=0, z=0}, {x=-1, y=0, z=0},
-			{x=0, y=0, z=1}, {x=0, y=0, z=1},
-		}
+		{ x = 0, y = 0, z = 0 },
+		{ x = 0, y = 1, z = 0 },
+		{ x = 0, y = -1, z = 0 },
+		{ x = 1, y = 0, z = 0 },
+		{ x = -1, y = 0, z = 0 },
+		{ x = 0, y = 0, z = 1 },
+		{ x = 0, y = 0, z = 1 },
+	}
 	local do_update = false
 	local old_value = 0
 	local timer
@@ -22,9 +25,11 @@ function wielded_light.update_light(pos, light_level)
 		if name == "air" and (minetest.get_node_light(light_pos) or 0) < light_level then
 			do_update = true
 			break
-		elseif name:sub(1,16) == "dynamic_lighting" then -- Update existing light node and timer
+		elseif name:sub(1, 16) == "dynamic_lighting" then -- Update existing light node and timer
 			old_value = tonumber(name:sub(18))
-			if not old_value or not light_level then return end
+			if not old_value or not light_level then
+				return
+			end
 			if light_level > old_value then
 				do_update = true
 			else
@@ -40,9 +45,9 @@ function wielded_light.update_light(pos, light_level)
 	if do_update then
 		timer = timer or minetest.get_node_timer(light_pos)
 		if light_level ~= old_value then
-			minetest.swap_node(light_pos, {name = "dynamic_lighting:"..light_level})
+			minetest.swap_node(light_pos, { name = "dynamic_lighting:" .. light_level })
 		end
-		timer:start(update_interval*3)
+		timer:start(update_interval * 3)
 	end
 end
 
@@ -65,10 +70,10 @@ function wielded_light.update_light_by_item(item, pos)
 	end
 end
 
-for i=1, 14 do
-	minetest.register_node("dynamic_lighting:"..i, {
+for i = 1, 14 do
+	minetest.register_node("dynamic_lighting:" .. i, {
 		drawtype = "airlike",
-		groups = {not_in_creative_inventory = 1},
+		groups = { not_in_creative_inventory = 1 },
 		walkable = false,
 		paramtype = "light",
 		sunlight_propagates = true,
@@ -77,22 +82,22 @@ for i=1, 14 do
 		buildable_to = true,
 		drops = {},
 		on_timer = function(pos, elapsed)
-			minetest.swap_node(pos, {name = "air"})
+			minetest.swap_node(pos, { name = "air" })
 		end,
 	})
 end
 
 local timer = 0
 minetest.register_globalstep(function(dtime)
-	timer = timer + dtime;
+	timer = timer + dtime
 	if timer < update_interval then
 		return
 	end
 	timer = 0
 
 	for _, player in pairs(minetest.get_connected_players()) do
-		local pos = vector.add (
-			vector.add({x = 0, y = 1, z = 0}, vector.round(player:get_pos())),
+		local pos = vector.add(
+			vector.add({ x = 0, y = 1, z = 0 }, vector.round(player:get_pos())),
 			vector.round(vector.multiply(player:get_velocity(), update_interval * 1.5))
 		)
 		wielded_light.update_light_by_item(player:get_wielded_item(), pos)
