2abc10d2d41f41fe299de8d2214608dac1014bf2
Farooq Karimi Zadeh <fkz@riseup.net>
2025-10-21 18:18:27 +0330

    improve formatting and update ctf_chat mod

--- mods/ctf/ctf_chat/init.lua
+++ mods/ctf/ctf_chat/init.lua
@@ -1,16 +1,16 @@
 ctf_chat = {}
 local callbacks = {}
 local prefix_callbacks = {}
-local colorize = minetest.colorize
+local colorize = core.colorize
 local SPECTATOR_CHAT_COLOR = "#8f7bb9"
 
-minetest.override_chatcommand("msg", {
+core.override_chatcommand("msg", {
 	func = function(name, param)
 		local sendto, message = param:match("^(%S+)%s(.+)$")
 		if not sendto then
 			return false, "Invalid usage, see /help msg."
 		end
-		if not minetest.get_player_by_name(sendto) then
+		if not core.get_player_by_name(sendto) then
 			return false, "The player " .. sendto .. " is not online."
 		end
 
@@ -21,43 +21,46 @@ minetest.override_chatcommand("msg", {
 		end
 
 		-- Message color
-		local color = minetest.settings:get("ctf_chat.message_color") or "#E043FF"
+		local color = core.settings:get("ctf_chat.message_color") or "#E043FF"
 		local pteam = ctf_teams.get(name)
 		local tcolor = pteam and ctf_teams.team[pteam].color or "#FFF"
 
 		-- Colorize the recepient-side message and send it to the recepient
-		local str =  colorize(color, "PM from ")
+		local str = colorize(color, "PM from ")
 		str = str .. colorize(tcolor, name)
 		str = str .. colorize(color, ": " .. message)
-		minetest.chat_send_player(sendto, str)
+		core.chat_send_player(sendto, str)
 
 		-- Make the sender-side message
 		str = "Message sent to " .. sendto .. ": " .. message
 
-		minetest.log("action", string.format("[CHAT] PM from %s to %s: %s", name, sendto, message))
+		core.log(
+			"action",
+			string.format("[CHAT] PM from %s to %s: %s", name, sendto, message)
+		)
 
 		-- Send the sender-side message
 		return true, str
-	end
+	end,
 })
 
-minetest.override_chatcommand("me", {
+core.override_chatcommand("me", {
 	func = function(name, param)
-		minetest.log("action", string.format("[CHAT] ME from %s: %s", name, param))
+		core.log("action", string.format("[CHAT] ME from %s: %s", name, param))
 
 		local pteam = ctf_teams.get(name)
 		if pteam then
 			local tcolor = ctf_teams.team[pteam].color
 			name = colorize(tcolor, "* " .. name)
 		else
-			name = "* ".. name
+			name = "* " .. name
 		end
 
-		minetest.chat_send_all(name .. " " .. param)
-	end
+		core.chat_send_all(name .. " " .. param)
+	end,
 })
 
-minetest.register_chatcommand("t", {
+core.register_chatcommand("t", {
 	params = "msg",
 	description = "Send a message on the team channel",
 	privs = { interact = true, shout = true },
@@ -68,20 +71,32 @@ minetest.register_chatcommand("t", {
 
 		local tname = ctf_teams.get(name)
 		if tname then
-			minetest.log("action", string.format("[CHAT] team message from %s (team %s): %s", name, tname, param))
+			core.log(
+				"action",
+				string.format(
+					"[CHAT] team message from %s (team %s): %s",
+					name,
+					tname,
+					param
+				)
+			)
 
 			local tcolor = ctf_teams.team[tname].color
 			for username in pairs(ctf_teams.online_players[tname].players) do
 				if not block_msgs or not block_msgs.is_chat_blocked(name, username) then
-					minetest.chat_send_player(username,
-							colorize(tcolor, "[TEAM] <" .. name .. "> ** " .. param .. " **"))
+					core.chat_send_player(
+						username,
+						colorize(tcolor, "[TEAM] <" .. name .. "> ** " .. param .. " **")
+					)
 				end
 			end
 		else
-			minetest.chat_send_player(name,
-					"You're not in a team, so you have no team to talk to.")
+			core.chat_send_player(
+				name,
+				"You're not in a team, so you have no team to talk to."
+			)
 		end
-	end
+	end,
 })
 
 function ctf_chat.register_on_chat_message_format(func)
@@ -90,7 +105,7 @@ end
 
 -- Register a prefix with a priority (lower number = higher priority)
 function ctf_chat.register_prefix(priority, func)
-	table.insert(prefix_callbacks, {priority = priority, func = func})
+	table.insert(prefix_callbacks, { priority = priority, func = func })
 	table.sort(prefix_callbacks, function(a, b)
 		return a.priority < b.priority
 	end)
@@ -112,12 +127,24 @@ end
 local function elysium_format_chat_message(name, message)
 	-- 1 index is staff ranks
 	local prefix = prefix_callbacks[1].func(name, "white") or ""
-	return joinStrings(core.colorize("#5a6d93", "[Elysium]:"), prefix, "<" .. name .. ">:", " ", message)
+	return joinStrings(
+		core.colorize("#5a6d93", "[Elysium]:"),
+		prefix,
+		"<" .. name .. ">:",
+		" ",
+		message
+	)
 end
 
 local function rush_spectator_format_chat_message(name, message)
 	local prefixes = format_prefixes(name, "white")
-	return joinStrings(core.colorize(SPECTATOR_CHAT_COLOR, "[Spectators]:"), prefixes, "<" .. name .. ">:", " ", message)
+	return joinStrings(
+		core.colorize(SPECTATOR_CHAT_COLOR, "[Spectators]:"),
+		prefixes,
+		"<" .. name .. ">:",
+		" ",
+		message
+	)
 end
 
 core.register_on_chat_message(function(name, message)
@@ -133,7 +160,10 @@ core.register_on_chat_message(function(name, message)
 			return true
 		end
 
-		core.log("action", string.format("[Rush Spectator Chat]: <%s>: %s", name, message))
+		core.log(
+			"action",
+			string.format("[Rush Spectator Chat]: <%s>: %s", name, message)
+		)
 
 		local formatted_msg = rush_spectator_format_chat_message(name, message)
 		local delivered = false
@@ -177,7 +207,7 @@ end)
 core.register_chatcommand("g", {
 	params = "msg",
 	description = "Send a message to the global chat",
-	privs = {interact = true, shout = true},
+	privs = { interact = true, shout = true },
 	func = function(name, message)
 		if message:trim() == "" then
 			return false, "You cannot send an empty message"
@@ -198,13 +228,13 @@ core.register_chatcommand("g", {
 			return true, "Message sent to the global chat."
 		end
 
-		core.chat_send_all(minetest.format_chat_message(name, message))
+		core.chat_send_all(core.format_chat_message(name, message))
 		return true, "Message sent to the global chat."
-	end
+	end,
 })
 
 -- Main chat message formatting function
-function minetest.format_chat_message(name, message)
+function core.format_chat_message(name, message)
 	if filter_caps then
 		message = filter_caps.parse(name, message)
 	end
@@ -215,12 +245,19 @@ function minetest.format_chat_message(name, message)
 		pteam_color = ctf_teams.team[pteam].color
 	end
 
-	local colorized_name = minetest.colorize(pteam_color, name)
+	local colorized_name = core.colorize(pteam_color, name)
 	local prefixes = format_prefixes(name, pteam_color)
-	local formatted_msg = joinStrings(prefixes, "<" .. colorized_name .. ">:", " ", message)
+	local formatted_msg =
+		joinStrings(prefixes, "<" .. colorized_name .. ">:", " ", message)
 
 	if not formatted_msg or #formatted_msg == 0 then
-		minetest.log("error", "[ctf_chat]: Chat message formatting failed! Player: " .. name .. " Raw msg: " .. message)
+		core.log(
+			"error",
+			"[ctf_chat]: Chat message formatting failed! Player: "
+				.. name
+				.. " Raw msg: "
+				.. message
+		)
 		return string.format("<%s>: %s", name or "unknown", message or "")
 	end
 
