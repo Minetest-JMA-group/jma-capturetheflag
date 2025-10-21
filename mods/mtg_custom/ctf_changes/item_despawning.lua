6070db2ad21745f436cee3c674bfdaa219419073
birdlover32767 <162155761+birdlover32767@users.noreply.github.com>
2025-10-21 19:01:03 +0300

    Do all requests

--- mods/mtg_custom/ctf_changes/item_despawning.lua
+++ mods/mtg_custom/ctf_changes/item_despawning.lua
@@ -1,8 +1,10 @@
+local DEFAULT_DESPAWN_TIME = 60
+local DESPAWN_CHECK_DELAY = 1
+
 -- This table represents how long items take to respawn
 -- Values of "true" don't automatically despawn
 
-local default_despawn_time = 45
-
+--- @type {[string]: any}
 local item_despawn_times = {
 	-- COMBAT ITEMS
 	["ctf_healing:bandage"] = true,
@@ -21,7 +23,7 @@ local item_despawn_times = {
 	["ctf_ranged:sniper_loaded"] = true,
 	["ctf_ranged:sniper_magnum"] = true,
 	["ctf_ranged:sniper_magnum_loaded"] = true,
-	["ctf_ranged:pistol_loaded"] = {base_despawn_time = 2 * 60},
+	["ctf_ranged:pistol_loaded"] = {base_despawn_time = 3 * 60},
 	["default:axe_mese"] = true,
 	["default:axe_diamond"] = true,
 	["default:shovel_mese"] = {base_despawn_time = 2 * 60},
@@ -36,10 +38,10 @@ local item_despawn_times = {
 	["grenades:smoke"] = true,
 	
 	-- ITEMS
-	["ctf_mode_chaos:power_charge"] = {base_despawn_time = 2 * 60, extra_time_per_item = 4},
+	["ctf_mode_chaos:power_charge"] = {base_despawn_time = 2 * 60, extra_time_per_item = 5},
 	["ctf_ranged:ammo"] = {extra_time_per_item = 3},
-	["default:blueberries"] = {base_despawn_time = 2 * 60, extra_time_per_item = 2},
-	["default:apple"] = {base_despawn_time = 2 * 60, extra_time_per_item = 2}, -- technically a node
+	["default:blueberries"] = {base_despawn_time = 3 * 60, extra_time_per_item = 3},
+	["default:apple"] = {base_despawn_time = 3 * 60, extra_time_per_item = 3}, -- technically a node
 	["default:diamond"] = true,
 	["default:mese_crystal"] = true,
 	["default:steel_ingot"] = {base_despawn_time = 2 * 60, extra_time_per_item = 7},
@@ -55,7 +57,7 @@ local item_despawn_times = {
 	-- QUICK DESPAWNING ITEMS
 	["ctf_combat:sword_stone"] = {base_despawn_time = 15},
 	["default:pick_stone"] = {base_despawn_time = 15},
-	["default:torch"] = {base_despawn_time = 15, extra_time_per_item = 0.5},
+	["default:torch"] = {base_despawn_time = 12, extra_time_per_item = 0.5},
 }
 
 
@@ -71,7 +73,18 @@ if not builtin_item or type(builtin_item.set_item) ~= "function" or type(builtin
 	return
 end
 
+--- @type {[string]: ObjectRef}
+local active_items = {}
+
 local item = {
+	check_for_despawn = function(self)
+		if self.timer then
+			self.timer = self.timer - DESPAWN_CHECK_DELAY
+			if self.timer <= 0 then
+				self.object:remove()
+			end
+		end
+	end,
 	set_item = function(self, itemstring, ...)
 		builtin_item.set_item(self, itemstring, ...)
 
@@ -82,31 +95,38 @@ local item = {
 			if modifiers == true then
 				self.timer = false
 			else
-				self.timer = (modifiers.base_despawn_time or default_despawn_time) + (modifiers.extra_time_per_item or 0) * stack:get_count() 
+				self.timer = (modifiers.base_despawn_time or DEFAULT_DESPAWN_TIME) + (modifiers.extra_time_per_item or 0) * stack:get_count() 
 			end
 		else
-			self.timer = default_despawn_time
+			self.timer = DEFAULT_DESPAWN_TIME
 		end
 	end,
-	on_step = function(self, dtime, ...)
-		builtin_item.on_step(self, dtime, ...)
-
-		if self.timer then
-			self.timer = self.timer - dtime
-			if self.timer < 0 then
-				self.object:remove()
-			end
+	on_activate = function(self, ...)
+		builtin_item.on_activate(self, ...)
+		active_items[self.object:get_guid()] = self
+		
+	end,
+	on_deactivate = function(self, removal, ...)
+		(builtin_item.on_deactivate or function() end)(self, removal, ...) -- future-proofed
+		active_items[self.object:get_guid()] = nil
+		if not removal then
+			self.object:remove()
 		end
 	end,
 }
 
-ctf_api.register_on_match_end(function()
-	-- Delete all items as soon as the match ends, shouldn't be a problem
-	for _, obj in pairs(core.objects_by_guid) do
-		local lua_entity = obj:get_luaentity()
-		if lua_entity and lua_entity.name == "__builtin:item" then
-			lua_entity.object:remove()
+local t = 0
+core.register_globalstep(function(dtime)
+	t = t + dtime
+	if t > DESPAWN_CHECK_DELAY then
+		for guid, ref in pairs(active_items) do
+			if ref.object:is_valid() then
+				ref:check_for_despawn()
+			else
+				active_items[guid] = nil
+			end
 		end
+		t = t - DESPAWN_CHECK_DELAY
 	end
 end)
 
