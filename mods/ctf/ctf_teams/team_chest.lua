4efaccad389fb3f82d146515095cabb1d03af32c
Farooq Karimi Zadeh <fkz@riseup.net>
2025-11-19 10:18:37 +0330

    Fix problem with crash

--- mods/ctf/ctf_teams/team_chest.lua
+++ mods/ctf/ctf_teams/team_chest.lua
@@ -321,7 +321,8 @@ for _, team in ipairs(ctf_teams.teamlist) do
 					local score = cur_mode.get_item_value(item_name, pos)
 					core.debug(
 						"We are supposed to give score for "
-							.. item_name(" and its value is ")
+							.. item_name
+							.. " and its value is "
 							.. tostring(score)
 					)
 					if score > 0 then
