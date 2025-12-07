35ba22400340eaa68a7973e7edc76663e2d5708b
Farooq Karimi Zadeh <fkz@riseup.net>
2025-12-07 18:52:42 +0330

    add another msg about killing a flag thief

--- mods/other/random_messages/init.lua
+++ mods/other/random_messages/init.lua
@@ -16,6 +16,7 @@ if core.settings:get("random_messages_disabled") == "true" then
 end
 
 local messages = {
+	S("You get more score for killing a flag thief than if they didn't have the flag"),
 	S(
 		"Want someone dead on enemy team for real? Try putting a bounty of 16 score on them: /bo targetName 16"
 	),
