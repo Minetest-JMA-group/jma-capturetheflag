007e0254bcb7f27907a94d91938122cddfd65ed5
Fhelron <fhelron@danielschlep.de>
2026-02-18 21:13:15 +0100

    immunity_overlay: Removed color overlay - only text

--- mods/ctf/ctf_modebase/immunity.lua
+++ mods/ctf/ctf_modebase/immunity.lua
@@ -72,13 +72,6 @@ function ctf_modebase.give_immunity(
 
 	immune_players[pname].finish_callback = finish_callback
 
-	--add immunity overlay which is scaled up over the whole screen
-	hud:add(player, "immunity_overlay", {
-		type = "image",
-		position = { x = 0.5, y = 0.5 },
-		image_scale = -300,
-		texture = "[combine:1x1^[invert:rgba^[opacity:1^[colorize:#85beff:101",
-	})
 	hud:add(player, "immunity_overlay_text", {
 		type = "text",
 		position = { x = 0.5, y = 0.1 },
@@ -119,7 +112,6 @@ function ctf_modebase.remove_immunity(player)
 
 	immune_players[pname] = nil
 
-	hud:remove(player, "immunity_overlay")
 	hud:remove(player, "immunity_overlay_text")
 
 	if finish_callback then
