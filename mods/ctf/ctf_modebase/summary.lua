03947bbb978a39f7df7987461832cd800e863e8b
Nanowolf4 <n4w@tutanota.com>
2026-06-19 17:27:55 +0300

    ctf_modebase/summary: Port UI improvements from upstream capturetheflag

--- mods/ctf/ctf_modebase/summary.lua
+++ mods/ctf/ctf_modebase/summary.lua
@@ -4,9 +4,9 @@ local winner = nil
 
 local player_sort_by = {}
 
-core.register_on_leaveplayer(function(player)
-	player_sort_by[player:get_player_name()] = nil
-end)
+local S = minetest.get_translator(minetest.get_current_modname())
+
+minetest.register_on_leaveplayer(function(player) player_sort_by[player:get_player_name()] = nil end)
 
 local function team_rankings(total)
 	local ranks = {}
@@ -14,7 +14,7 @@ local function team_rankings(total)
 	for team, rank_values in pairs(total) do
 		rank_values._row_color = ctf_teams.team[team].color
 
-		ranks[HumanReadable("team " .. team)] = rank_values
+		ranks[HumanReadable(S("team") .." ".. team)] = rank_values
 	end
 
 	return ranks
@@ -22,47 +22,40 @@ end
 
 ctf_modebase.summary = {}
 
----@overload fun(prev): nil
----@return unknown, table, unknown, { title: string, special_row_title: string, game_stat: unknown?, winner: unknown?, duration: string, buttons: {previous: boolean}, allow_sort: boolean}
 function ctf_modebase.summary.get(prev)
 	if not prev then
 		local current_mode = ctf_modebase:get_current_mode()
-		if not current_mode then
-			return
-		end
+		if not current_mode then return end
 		local rankings = current_mode.recent_rankings
 
-		return rankings.players(),
-			team_rankings(rankings.teams()),
-			current_mode.summary_ranks,
-			{
-				title = "Match Summary",
-				special_row_title = "Total Team Stats",
+		return
+			rankings.players(), team_rankings(rankings.teams()), current_mode.summary_ranks, {
+				title = S("Match Summary"),
+				special_row_title = S("Total Team Stats"),
 				game_stat = game_stat,
 				winner = winner,
 				duration = ctf_map.get_duration(),
-				buttons = { previous = previous ~= nil },
+				map = ctf_map.current_map.name,
+				buttons = {previous = previous ~= nil},
 				allow_sort = true,
 			}
 	elseif previous ~= nil then
-		return previous.players,
-			team_rankings(previous.teams),
-			previous.summary_ranks,
-			{
-				title = "Previous Match Summary",
-				special_row_title = "Total Team Stats",
+		return
+			previous.players, team_rankings(previous.teams), previous.summary_ranks, {
+				title = S("Previous Match Summary"),
+				special_row_title = S("Total Team Stats"),
 				game_stat = previous.game_stat,
 				winner = previous.winner,
 				duration = previous.duration,
-				buttons = { next = true },
+				map = previous.map,
+				buttons = {next = true},
 				allow_sort = true,
 			}
 	end
 end
 
 ctf_api.register_on_new_match(function()
-	game_stat = string.format(
-		"%s mode: Round %d of %d",
+	game_stat = S("@1 mode: Round @2 of @3",
 		HumanReadable(ctf_modebase.current_mode),
 		ctf_modebase.current_mode_matches_played + 1,
 		ctf_modebase.current_mode_matches
@@ -71,17 +64,16 @@ end)
 
 ctf_api.register_on_match_end(function()
 	local current_mode = ctf_modebase:get_current_mode()
-	if not current_mode then
-		return
-	end
+	if not current_mode then return end
 	local rankings = current_mode.recent_rankings
 
 	previous = {
 		players = rankings.players(),
 		teams = rankings.teams(),
 		game_stat = game_stat,
-		winner = winner or "NO WINNER",
+		winner = winner or S("NO WINNER"),
 		duration = ctf_map.get_duration(),
+		map = ctf_map.current_map.name,
 		summary_ranks = current_mode.summary_ranks,
 	}
 
@@ -96,13 +88,7 @@ end
 ---@param rankings table Recent rankings to show in the gui
 ---@param rank_values table Example: `{_sort = "score", "captures" "kills"}`
 ---@param formdef table table for customizing the formspec
-function ctf_modebase.summary.show_gui(
-	name,
-	rankings,
-	special_rankings,
-	rank_values,
-	formdef
-)
+function ctf_modebase.summary.show_gui(name, rankings, special_rankings, rank_values, formdef)
 	local sort_by = rank_values._sort or rank_values[1]
 
 	local sort = function(unsorted)
@@ -115,25 +101,16 @@ function ctf_modebase.summary.show_gui(
 			table.insert(sorted, t)
 		end
 
-		table.sort(sorted, function(a, b)
-			return a.sort > b.sort
-		end)
+		table.sort(sorted, function(a, b) return a.sort > b.sort end)
 
 		return sorted
 	end
 
-	ctf_modebase.summary.show_gui_sorted(
-		name,
-		sort(rankings),
-		sort(special_rankings),
-		rank_values,
-		formdef
-	)
+	ctf_modebase.summary.show_gui_sorted(name, sort(rankings), sort(special_rankings), rank_values, formdef)
 end
 
 local function show_for_player(name, prev)
-	local match_rankings, special_rankings, rank_values, formdef =
-		ctf_modebase.summary.get(prev)
+	local match_rankings, special_rankings, rank_values, formdef = ctf_modebase.summary.get(prev)
 	if not match_rankings then
 		return false
 	end
@@ -142,15 +119,7 @@ local function show_for_player(name, prev)
 		rank_values._sort = player_sort_by[name]
 	end
 
-	local sr = special_rankings
-	local rv = rank_values
-	ctf_modebase.summary.show_gui(
-		name,
-		match_rankings,
-		sr,
-		rv,
-		formdef
-	)
+	ctf_modebase.summary.show_gui(name, match_rankings, special_rankings, rank_values, formdef)
 	return true
 end
 
@@ -158,19 +127,9 @@ end
 ---@param rankings table Sorted recent rankings Example: `{{pname=a, score=2}, {pname=b, score=1}}`
 ---@param rank_values table Example: `{_sort = "score", "captures" "kills"}`
 ---@param formdef table table for customizing the formspec
-function ctf_modebase.summary.show_gui_sorted(
-	name,
-	rankings,
-	special_rankings,
-	rank_values,
-	formdef
-)
-	if not formdef then
-		formdef = {}
-	end
-	if not formdef.buttons then
-		formdef.buttons = {}
-	end
+function ctf_modebase.summary.show_gui_sorted(name, rankings, special_rankings, rank_values, formdef)
+	if not formdef then formdef = {} end
+	if not formdef.buttons then formdef.buttons = {} end
 
 	local sort_by_idx = table.indexof(rank_values, rank_values._sort)
 
@@ -200,13 +159,8 @@ function ctf_modebase.summary.show_gui_sorted(
 			end
 
 			local row = string.format(
-				"%d,%s,%s,%s" .. ",%s,%s",
-				ranks.number or i,
-				color,
-				ranks.pname,
-				color,
-				math.round(ranks[rank_values[sort_by_idx]] or 0),
-				color
+				"%d,%s,%s,%s"..",%s,%s",
+				ranks.number or i, color, ranks.pname, color, math.round(ranks[rank_values[sort_by_idx]] or 0), color
 			)
 			local rv = table.copy(rank_values)
 			table.remove(rv, sort_by_idx)
@@ -224,138 +178,206 @@ function ctf_modebase.summary.show_gui_sorted(
 
 	if #special_rankings >= 1 then
 		if formdef.special_row_title then
-			table.insert(
-				special_rankings,
-				1,
-				string.format(
-					",white,%s,cyan,%s,white,%s",
-					formdef.special_row_title,
-					HumanReadable(sortby),
-					HumanReadable(table.concat(modified_ranks, "  ,"))
-				)
-			)
+			table.insert(special_rankings, 1, string.format(
+				",white,%s,cyan,%s,white,%s",
+				formdef.special_row_title, HumanReadable(sortby), HumanReadable(table.concat(modified_ranks, "  ,"))
+			))
 		end
 
-		table.insert(special_rankings, string.rep(",", #modified_ranks + 6))
+		table.insert(special_rankings, string.rep(",", #modified_ranks+6))
 	end
 
-	local formspec = {
-		title = formdef.title or "Summary",
-		elements = {
-			rankings = {
-				type = "table",
-				pos = { 0.1, 1 + (formdef.allow_sort and 1 or 0) },
-				size = {
-					math.max(
-						ctf_gui.FORM_SIZE.x - 0.2,
-						(1 + 8 + 16 + table.concat(rank_values, "  ,"):len()) * 0.3
-					),
-					(ctf_gui.FORM_SIZE.y - (formdef.allow_sort and 2 or 1))
-						- (ctf_gui.ELEM_SIZE.y + 3),
-				},
-				options = {
-					highlight = "#00000000",
-				},
-				columns = {
-					{ type = "text", width = 1 },
-					{ type = "color" }, -- Player team color
-					{ type = "text", width = 16 }, -- Player name
-					"color", -- sortby text color
-					"text", -- sortby text
-					"color", -- Reset color
-					("text;"):rep(#modified_ranks):sub(1, -2),
-				},
-				rows = {
-					#special_rankings > 1 and table.concat(special_rankings, ",") or "",
-					"white",
-					"Player Name",
-					"cyan",
-					HumanReadable(sortby) .. "  ",
-					"white",
-					HumanReadable(table.concat(modified_ranks, "  ,")),
-					table.concat(rankings, ","),
-				},
+	ctf_gui.show_formspec(name, "ctf_modebase:summary", function(ctx)
+		local winfo = core.get_player_window_information(ctx.pname)
+
+		local TWO_START_COLS_LEN = 28
+		local tlen = ctx.sortby:len() + TWO_START_COLS_LEN
+
+		for _, str in pairs(ctx.modified_ranks) do
+			tlen = tlen + math.max(6, str:len()) + 0.333
+		end
+
+		-- target: 0.6555
+		local FORM_X = 18.7
+		local FORM_Y = 12.5
+
+		if winfo then
+			FORM_X = (FORM_X > winfo.max_formspec_size.x) and winfo.max_formspec_size.x or FORM_X
+			FORM_Y = (FORM_Y > winfo.max_formspec_size.y) and winfo.max_formspec_size.y or FORM_Y
+		end
+
+		local EDITOR_X = 1920
+		local EDITOR_Y = 1080
+
+		if not winfo then
+			winfo = {size = {x = EDITOR_X, y = EDITOR_Y}, max_formspec_size = {x = 28, y = 15}}
+		end
+
+		local SCALE_SIZE_X = 0.14 * (EDITOR_X / winfo.size.x) * (winfo.max_formspec_size.x / FORM_X)
+		local SCALE_SIZE_Y = 0.39 * (EDITOR_Y / winfo.size.y) * (winfo.max_formspec_size.y / FORM_Y)
+
+		local EXTRA_HEIGHT = (ctx.formdef.winner and 1 or 0) +
+				(ctx.formdef.game_stat and 1 or 0) +
+				(ctx.formdef.duration and 1 or 0)
+		local EXTRA_HEIGHT_INVERSE = 3 - EXTRA_HEIGHT
+		local EXTRA_HEADER = #ctx.rankings >= 20
+
+		local BOTH_BUTTONS = ctx.formdef.buttons.next and ctx.formdef.buttons.previous
+		local NEITHER_BUTTONS = not ctx.formdef.buttons.next and not ctx.formdef.buttons.previous
+		local FILL_FORM = 0
+
+		if NEITHER_BUTTONS then
+			FILL_FORM = 1.5
+		end
+
+		local out = {
+			"formspec_version[8]",
+			{"size[%.1f,%.1f;true]", FORM_X, FORM_Y},
+			"padding[0,0]",
+			{
+				"label[0.5,2;%s%s%s]",
+				(ctx.formdef.winner and (ctx.formdef.winner .. "\n") or ""),
+				(ctx.formdef.game_stat and (ctx.formdef.game_stat .. "\n") or ""),
+				(ctx.formdef.duration and (S("Duration") ..": ".. ctx.formdef.duration) or ""),
 			},
-		},
-	}
+			{"hypertext[0,0.2;%.1f,1.6;title;<center><big>", FORM_X},
+				core.formspec_escape((ctx.formdef.title or S("Summary")) .. (ctx.formdef.map and " - "..ctx.formdef.map or "")),
+			"</big></center>]",
+			{
+				"scroll_container[0.2,%.1f;%.1f,%.1f;formcontenty;vertical;0.1;0]",
+				1 + EXTRA_HEIGHT,
+				FORM_X - 0.8,
+				(FORM_Y - 6.3) + EXTRA_HEIGHT_INVERSE + FILL_FORM
+			},
+			{
+				"scroll_container[0,0;%.1f,%.1f;formcontentx;horizontal;0.1;0]",
+				FORM_X - 0.8,
+				math.max( -- SCROLL HEIGHT
+					(FORM_Y - 6.3) + EXTRA_HEIGHT_INVERSE,
+					(#ctx.rankings + #ctx.special_rankings) * SCALE_SIZE_Y
+				) + FILL_FORM,
+			},
+			"tableoptions[highlight=#00000000;border=false]",
+			string.format(
+				"tablecolumns[text,width=4;color;text,width=18;color;text,width=6;color;%s]",
+				("text,width=6;"):rep(#ctx.modified_ranks):sub(1, -2)
+			),
+			"style[rankings;font_size=14;font=mono]",
+			string.format(
+				"table[0,0;%.1f,%.1f;rankings;%s,%s%s;1]",
+				math.max(
+					FORM_X - 0.2,
+					tlen * SCALE_SIZE_X -- SCROLL WIDTH
+				),
+				--
+				-- Height of table, should always be big enough to fit all entries,
+				-- will get cut to size by scroll container
+				math.max(#ctx.rankings, FORM_Y),
+				#ctx.special_rankings > 1 and table.concat(ctx.special_rankings, ",") or "",
+				table.concat({
+					"white",
+					S("Player Name"),
+					"cyan", HumanReadable(ctx.sortby).."  ", "white",
+					HumanReadable(table.concat(ctx.modified_ranks, "  ,")),
+					table.concat(ctx.rankings, ","),
+				}, ","),
+				EXTRA_HEADER and table.concat({
+					",,white",
+					S("Player Name"),
+					"cyan", HumanReadable(ctx.sortby).."  ", "white",
+					HumanReadable(table.concat(ctx.modified_ranks, "  ,")),
+				}, ",") or ""
+			),
+			"scroll_container_end[]",
+			"scroll_container_end[]",
+			{
+				"scrollbar[%.1f,%.1f;0.4,%.1f;vertical;formcontenty;0]",
+				FORM_X - 0.5,
+				EXTRA_HEIGHT + 1,
+				(FORM_Y - 6.3) + EXTRA_HEIGHT_INVERSE + FILL_FORM
+			},
+			{
+				"scrollbar[0.2,%.1f;%.1f,0.4;horizontal;formcontentx;0]",
+				FORM_Y - 2.2 + FILL_FORM,
+				FORM_X - 0.7,
+			},
+		}
 
-	if formdef.allow_sort then
-		formspec.elements.sorting = {
-			type = "dropdown",
-			items = rank_values,
-			default_idx = sort_by_idx,
-			give_idx = false,
-			pos = { x = 13, y = 1 },
-			size = { x = ctf_gui.ELEM_SIZE.x + 1, y = ctf_gui.ELEM_SIZE.y },
-			func = function(playername, fields, field_name)
-				if
-					fields.sorting
-					and sortby ~= fields.sorting
-					and table.indexof(rank_values, fields.sorting) ~= -1
-				then
-					player_sort_by[playername] = fields.sorting
-					show_for_player(playername, formdef.buttons.next and true or false)
+		if ctx.formdef.allow_sort then
+			local x = FORM_X - (ctf_gui.ELEM_SIZE.x + 1.5)
+			local y = 1.8
+			table.insert(out, string.format(
+				"dropdown[%.1f,%.1f;%.1f,%.1f;sorting;%s;1;false]",
+				x, y + 0.2,
+				ctf_gui.ELEM_SIZE.x + 1,
+				ctf_gui.ELEM_SIZE.y,
+				table.concat(ctx.rank_values, ",")
+			))
+
+			table.insert(out, string.format(
+				"label[%.1f,%.1f;%s: ]",
+				x, y,
+				S("Sort players by")
+			))
+		end
+
+		local x_pos = FORM_X / 2 - 2.5
+
+		if BOTH_BUTTONS then x_pos = (FORM_X / 2 - 2.5) + 3 end
+
+		if ctx.formdef.buttons.next then
+			table.insert(out,  string.format(
+				"button[%.1f,%.1f;5,1;next;%s]",
+				x_pos,
+				FORM_Y - 1.3,
+				BOTH_BUTTONS and S("See Next") or S("See Current")
+			))
+		end
+
+		if BOTH_BUTTONS then x_pos = (FORM_X / 2 - 2.5) - 3 end
+
+		if ctx.formdef.buttons.previous then
+			table.insert(out, string.format(
+				"button[%.1f,%.1f;5,1;previous;%s]",
+				x_pos,
+				FORM_Y - 1.3,
+				S("See Previous")
+			))
+		end
+
+		return ctf_gui.list_to_formspec_str(out)
+	end,
+	{
+		formdef = formdef,
+		rank_values = rank_values,
+		rankings = rankings,
+		special_rankings = special_rankings,
+		modified_ranks = modified_ranks,
+		sortby = sortby,
+		pname = name,
+		_on_formspec_input = function(pname, context, fields)
+			if context.formdef.buttons.previous and fields.previous then
+				show_for_player(pname, true)
+			end
+
+			if context.formdef.buttons.next and fields.next then
+				show_for_player(pname, false)
+			end
+
+			if context.formdef.allow_sort then
+				if fields.sorting and sortby ~= fields.sorting and table.indexof(rank_values, fields.sorting) ~= -1 then
+					player_sort_by[pname] = fields.sorting
+					show_for_player(pname, context.formdef.buttons.next and true or false)
 				end
-			end,
-		}
-		formspec.elements.label = {
-			type = "label",
-			pos = { 13, 0.5 },
-			label = "Sort players by: ",
-		}
-	end
+			end
+		end,
+	})
 
-	if formdef.buttons.next then
-		formspec.elements.next = {
-			type = "button",
-			label = "See Current",
-			pos = { "center", ctf_gui.FORM_SIZE.y - (ctf_gui.ELEM_SIZE.y + 2.5) },
-			func = function()
-				show_for_player(name, false)
-			end,
-		}
-	end
-
-	if formdef.buttons.previous then
-		formspec.elements.previous = {
-			type = "button",
-			label = "See Previous",
-			pos = { "center", ctf_gui.FORM_SIZE.y - (ctf_gui.ELEM_SIZE.y + 2.5) },
-			func = function()
-				show_for_player(name, true)
-			end,
-		}
-	end
-
-	if formdef.game_stat then
-		formspec.elements.game_stat = {
-			type = "label",
-			pos = { 1, 0.5 },
-			label = formdef.game_stat,
-		}
-	end
-
-	if formdef.winner then
-		formspec.elements.winner = {
-			type = "label",
-			pos = { 5, 1.3 },
-			label = formdef.winner,
-		}
-	end
-
-	if formdef.duration then
-		formspec.elements.duration = {
-			type = "label",
-			pos = { 1, 1.3 },
-			label = "Duration: " .. formdef.duration,
-		}
-	end
-
-	ctf_gui.old_show_formspec(name, "ctf_modebase:summary", formspec)
+	minetest.log("action", "[ctf_modebase.summary] Showed gui to "..dump(name))
 end
 
 ctf_core.register_chatcommand_alias("summary", "s", {
-	description = "Show a summary for the current match",
+	description = S("Show a summary for the current match"),
 	func = function(name, param)
 		local prev
 		if not param or param == "" then
@@ -363,13 +385,13 @@ ctf_core.register_chatcommand_alias("summary", "s", {
 		elseif param:match("p") then
 			prev = true
 		else
-			return false, "Can't understand param " .. dump(param)
+			return false, S("Can't understand param") .." ".. dump(param)
 		end
 
 		if not show_for_player(name, prev) then
-			return false, "No match summary!"
+			return false, S("No match summary!")
 		end
 
 		return true
-	end,
+	end
 })
