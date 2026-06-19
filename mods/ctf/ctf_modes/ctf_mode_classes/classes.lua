d578e544a910543f12e69de44b3be48af92dd719
Nanowolf4 <n4w@tutanota.com>
2026-06-19 18:51:50 +0300

    ctf_mode_classes: Port UI improvements from upstream capturetheflag
    Co-authored-by: LandarVargan <lonewolf04361@gmail.com>

--- mods/ctf/ctf_modes/ctf_mode_classes/classes.lua
+++ mods/ctf/ctf_modes/ctf_mode_classes/classes.lua
@@ -5,13 +5,16 @@ local CLASS_SWITCH_COOLDOWN = 30
 
 local classes = {}
 
-local class_list = { "knight", "ranged", "support" }
+local S = minetest.get_translator(minetest.get_current_modname())
+
+local class_list = {"knight", "ranged", "support"}
 local class_props = {
 	knight = {
 		name = "Knight",
-		description = "High HP class with a sword capable of short damage bursts",
+		color = "grey",
+		description = S("High HP class with a sword capable of short damage bursts"),
 		hp_max = 30,
-		visual_size = vector.new(1.1, 1.05, 1.1),
+		visual_size = vector.new(0.1, 0.05, 0.1),
 		items = {
 			"ctf_mode_classes:knight_sword",
 		},
@@ -26,7 +29,8 @@ local class_props = {
 	},
 	support = {
 		name = "Support",
-		description = "Helper class with healing bandages, an immunity ability, and building gear",
+		color = "cyan",
+		description = S("Helper class with healing bandages, an immunity ability, and building gear"),
 		physics = { speed = 1.1 },
 		items = {
 			"ctf_mode_classes:support_bandage",
@@ -42,14 +46,15 @@ local class_props = {
 			"ctf_ranged:desert_eagle",
 			"ctf_ranged:minigun",
 		},
-		disallowed_items_textures = {
+		disallowed_items_markup = {
 			["ctf_melee:"] = "default_tool_steelsword.png^ctf_modebase_group.png",
 		},
 	},
 	ranged = {
 		name = "Scout",
-		description = "Ranged class with a scoped rifle/grenade launcher and a scaling ladder for reaching high places",
-		visual_size = vector.new(0.9, 1, 0.9),
+		color = "orange",
+		description = S("Ranged class with a scoped rifle/grenade launcher and a scaling ladder for reaching high places"),
+		visual_size = vector.new(-0.1, 0.0, -0.1),
 		items = {
 			"ctf_mode_classes:ranged_rifle_loaded",
 			"ctf_mode_classes:scaling_ladder",
@@ -58,55 +63,41 @@ local class_props = {
 			"ctf_melee:",
 			"ctf_healing:healing_pistol",
 		},
-		disallowed_items_textures = {
+		disallowed_items_markup = {
 			["ctf_melee:"] = "default_tool_steelsword.png^ctf_modebase_group.png",
 		},
 	},
 }
 
-core.register_on_mods_loaded(function()
+minetest.register_on_mods_loaded(function()
 	for k, class_prop in pairs(class_props) do
 		local items_markup = ""
 		local disallowed_items_markup = ""
 
 		for _, iname in ipairs(class_prop.items or {}) do
 			local item = ItemStack(iname)
-			local count = item:get_count()
 
-			if count <= 1 then
-				---@diagnostic disable-next-line: cast-local-type
-				count = nil
-			else
-				---@diagnostic disable-next-line: cast-local-type
-				count = " x" .. count
-			end
-
-			local desc = string.split(item:get_description(), "\n", false, 1)
-			items_markup = string.format(
-				"%s%s\n<item name=%s float=left width=48>\n\n\n",
+			items_markup = string.format("%s <item name=%s width=48>",
 				items_markup,
-				core.formspec_escape(desc[1]) .. (count and count or ""),
 				item:get_name()
 			)
 		end
 
 		for _, iname in ipairs(class_prop.disallowed_items or {}) do
-			if core.registered_items[iname] then
-				disallowed_items_markup = string.format(
-					"%s<item name=%s width=48>",
+			if minetest.registered_items[iname] then
+				disallowed_items_markup = string.format("%s <item name=%s width=48>",
 					disallowed_items_markup,
 					iname
 				)
 			else
-				disallowed_items_markup = string.format(
-					"%s<img name=%s width=48>",
+				disallowed_items_markup = string.format("%s <img name=%s width=48>",
 					disallowed_items_markup,
-					class_prop.disallowed_items_textures[iname]
+					class_prop.disallowed_items_markup[iname]
 				)
 			end
 		end
 
-		class_props[k].items_markup = items_markup:sub(1, -2) -- Remove \n at the end of str
+		class_props[k].items_markup = items_markup.."\n"
 		class_props[k].disallowed_items_markup = disallowed_items_markup
 	end
 end)
@@ -398,12 +389,12 @@ ctf_healing.register_bandage("ctf_mode_classes:support_bandage", {
 })
 
 function classes.get_name(player)
-	local meta = player:get_meta()
+	local pname = player:get_player_name()
 
-	local cname = meta:get_string("class")
+	local cname = ctf_core.meta_get_string(pname, "class")
 	if not cname or not class_props[cname] then
 		cname = "knight"
-		meta:set_string("class", cname)
+		ctf_core.meta_set_string(pname, "class", cname)
 	end
 
 	return cname
@@ -414,23 +405,23 @@ function classes.get(player)
 end
 
 function classes.get_skin_overlay(player_or_class, class)
-	return "^ctf_mode_classes_"
-		.. (class and player_or_class or classes.get_name(player_or_class))
-		.. "_overlay.png"
+	return "^ctf_mode_classes_" .. (class and player_or_class or classes.get_name(player_or_class)) .. "_overlay.png"
 end
 
 function classes.update(player)
 	local class = classes.get(player)
+	local base_size = player_api.registered_models['character.b3d'].visual_size
+	base_size = vector.new(base_size.x, base_size.y, base_size.z or base_size.x)
 
 	player:set_properties({
-		hp_max = class.hp_max or core.PLAYER_MAX_HP_DEFAULT,
-		visual_size = class.visual_size or vector.new(1, 1, 1),
+		hp_max = class.hp_max or minetest.PLAYER_MAX_HP_DEFAULT,
+		visual_size = vector.add(base_size, class.visual_size or vector.new()) or base_size
 	})
 
 	if class.physics then
 		physics.set(player:get_player_name(), "ctf_mode_classes:class_physics", {
-			speed = class.physics.speed or 1,
-			jump = class.physics.jump or 1,
+			speed   = class.physics.speed or 1,
+			jump    = class.physics.jump or 1,
 			gravity = class.physics.gravity or 1,
 		})
 	else
@@ -438,12 +429,22 @@ function classes.update(player)
 	end
 end
 
+classes.registered_on_class_change = {}
+---@param func(player, class, oldclass)
+function classes.register_on_class_change(func)
+	table.insert(classes.registered_on_class_change, func)
+end
+
 function classes.set(player, classname)
-	if classname == classes.get_name(player) then
+	local oldclass = classes.get_name(player)
+
+	if classname == oldclass then
 		return
 	end
 
-	player:get_meta():set_string("class", classname)
+	RunCallbacks(classes.registered_on_class_change, player, oldclass)
+
+	ctf_core.meta_set_string(player:get_player_name(), "class", classname)
 
 	ctf_modebase.update_wear.cancel_player_updates(player)
 
@@ -459,7 +460,9 @@ end
 
 local function select_class(player, classname)
 	player = PlayerObj(player)
-	if not player then
+	if not player then return end
+
+	if classname == classes.get_name(player) then
 		return
 	end
 
@@ -481,172 +484,166 @@ end
 
 function classes.show_class_formspec(player)
 	player = PlayerObj(player)
-	if not player then
+	if not player then return end
+
+	if ctf_modebase.current_mode ~= "classes" then return end
+
+	if dist_from_flag(player) > 5 then
+		hud_events.new(player, {
+			quick = true,
+			text = S("You can only change class at your flag!"),
+			color = "warning",
+		})
 		return
 	end
 
-	if not cooldowns:get(player) then
-		if ctf_modebase.current_mode ~= "classes" then
-			return
-		end
+	local pteam = ctf_teams.get(player)
 
-		if dist_from_flag(player) > 5 then
-			hud_events.new(player, {
-				quick = true,
-				text = "You can only change class at your flag!",
-				color = "warning",
+	ctf_gui.show_formspec(player, "ctf_mode_classes:class_form", function(context)
+		local form_x, form_y = 14, 10
+		local pad = 0.3
+
+		local bw = 3
+
+		local class = context.class
+		local class_prop = context.class_props[class]
+
+		local out = {
+			"formspec_version[4]",
+			{"size[%f,%f]", form_x, form_y+1.1},
+			"real_coordinates[true]",
+			{"hypertext[0,0.2;%f,1.6;title;<big><center><b>Class Info: %s</b></center></big>]", form_x, class_prop.name},
+
+			{"box[%s,1.2;%f,%f;#00000077]", pad, ((form_x/2)-0.7) - pad, form_y-2.4},
+			{"model[%s,1.4;%f,%f;classpreview;character.b3d;%s,blank.png;{0,160};;;]",
+				pad,
+				((form_x/2)-0.7) - pad,
+				form_y-2.6,
+				ctf_cosmetics.get_colored_skin(context.player, context.pteam and ctf_teams.team[context.pteam].color) ..
+						context.classes.get_skin_overlay(class, true) or ""
+			},
+			{[[hypertext[%f,1.2;%f,%f;info;<global margin=20 font=mono background=#00000044>
+				</b>
+				<center>%s</center>
+
+
+				<img name=heart.png width=20 float=left> %d HP
+				%s
+				Special items
+				%s
+				Disallowed Items
+				%s
+				] ]],
+				(form_x/2)-0.6,
+				(form_x/2)+0.6 - pad,
+				form_y-2.4,
+				class_prop.description,
+				class_prop.hp_max or minetest.PLAYER_MAX_HP_DEFAULT,
+				class_prop.physics and class_prop.physics.speed and
+						"<img name=sprint_stamina_icon.png width=20 float=left> "..class_prop.physics.speed.."x Speed\n" or "",
+				class_prop.items_markup,
+				class_prop.disallowed_items_markup
+			},
+		}
+
+		local tb = #context.class_list -- total buttons
+		local active_class = classes.get_name(player)
+		for i, c in pairs(context.class_list) do
+			local sect = (i-1)/(tb-1)
+			local font_color = "#ffffff"
+			local is_active = (c == active_class)
+			local button_label = context.class_props[c].name
+			if cooldowns:get(player) then
+				font_color = "#f23f42"
+				table.insert(out,
+					{"tooltip[select_%s;You can only change your class every "..CLASS_SWITCH_COOLDOWN.." seconds. ]", c}
+				)
+				if is_active then
+					button_label = "▶ " .. button_label
+				end
+			elseif is_active then
+				font_color = "#ffff00"
+				button_label = "▶ " .. button_label
+			end
+			table.insert(out, {
+				"style[select_%s;textcolor=".. font_color ..
+				";font_size=*1.4;content_offset=-%f,0;bgcolor="..context.class_props[c].color.."]" ..
+				"style[show_%s;padding=8,8;bgcolor="..context.class_props[c].color.."]",
+				c,
+				20 + 8,
+				c,
 			})
-			return
-		end
-
-		local pteam = ctf_teams.get(player)
-
-		ctf_gui.show_formspec(player, "ctf_mode_classes:class_form", function(context)
-			local form_x, form_y = 12, 10
-
-			local bar_h = 2.2
-			local bw = 5
-
-			local class = context.class
-			local class_prop = class_props[class]
-
-			local next_class = class_props[class_list[wrap_class(
-				table.indexof(class_list, class) + 1
-			)]].name
-			local prev_class = class_props[class_list[wrap_class(
-				table.indexof(class_list, class) - 1
-			)]].name
-
-			return ctf_gui.list_to_formspec_str({
-				"formspec_version[4]",
-				{ "size[%f,%f]", form_x, form_y + 1.1 },
-				"real_coordinates[true]",
-				{
-					"hypertext[0,0.2;%f,1.3;title;<bigger><center><b>Class Selection</b></center></bigger>]",
-					form_x,
-				},
-
-				{
-					"hypertext[0,%f;%f,1;classname;<bigger><center><style color=#0DD>%s</style></center></bigger>]",
-					bar_h - 0.9,
-					form_x,
-					class_prop.name,
-				},
-				{ "box[0,%f;%f,0.8;#00000022]", bar_h - 0.9, form_x },
-
-				{
-					"image_button[0.1,%f;0.8,0.8;creative_prev_icon.png;prev_class;]",
-					bar_h - 0.9,
-				},
-				{
-					"hypertext[1,%f;%f,1;classprev;<left><big><style color=#0DD>%s</style></big></left>]",
-					bar_h - 0.7,
-					(form_x / 2) - 1,
-					prev_class,
-				},
-
-				{
-					"image_button[%f,%f;0.8,0.8;creative_next_icon.png;next_class;]",
-					form_x - 0.9,
-					bar_h - 0.9,
-				},
-				{
-					"hypertext[%f,%f;%f,1;classprev;<right><big><style color=#0DD>%s</style></big></right>]",
-					(form_x / 2),
-					bar_h - 0.7,
-					(form_x / 2) - 1,
-					next_class,
-				},
-
-				{ "box[0.1,2.3;%f,%f;#00000077]", (form_x / 2) - 0.8, form_y - 2.4 },
-				{
-					"model[0.1,2.3;%f,%f;classpreview;character.b3d;%s,blank.png;{0,160};;;]",
-					(form_x / 2) - 0.8,
-					form_y - 2.4,
-					(ctf_cosmetics.get_colored_skin(
-						player,
-						pteam and ctf_teams.team[pteam].color
-					) .. classes.get_skin_overlay(class, true)) or "",
-				},
-				{
-					[[hypertext[%f,2.3;%f,%f;info;<global font=mono background=#00000044>
-					<center>%s</center>
-					<img name=heart.png width=20 float=left> %d HP
-					%s
-					%s
-					Disallowed Items
-					%s
-					] ]],
-					(form_x / 2) - 0.6,
-					(form_x / 2) + 0.5,
-					form_y - 2.4,
-					class_prop.description,
-					class_prop.hp_max or core.PLAYER_MAX_HP_DEFAULT,
-					class_prop.physics
-							and class_prop.physics.speed
-							and "<img name=sprint_stamina_icon.png width=20 float=left> " .. class_prop.physics.speed .. "x Speed\n"
-						or "",
-					class_prop.items_markup,
-					class_prop.disallowed_items_markup,
-				},
-				"style[select;font_size=*1.5]",
-				{
-					"button_exit[%f,%f;%f,1;select;Choose Class]",
-					(form_x / 2) - (bw / 2),
-					form_y,
+			if not cooldowns:get(player) then
+				table.insert(out, {
+					"button_exit[%f,%f;%f,1;select_%s;%s]",
+					pad + (((form_x-(pad*2 + bw))) * sect),
+					form_y-0.5,
 					bw,
-				},
+					c,
+					button_label,
+				})
+			else
+				table.insert(out, {
+					"button[%f,%f;%f,1;select_%s;%s]",
+					pad + (((form_x-(pad*2 + bw))) * sect),
+					form_y-0.5,
+					bw,
+					c,
+					button_label,
+				})
+			end
+			table.insert(out, {
+				"image_button[%f,%f;1,1;settings_info.png;show_%s;]",
+				pad + (((form_x-(pad*2 + bw))) * sect) + bw - 1,
+				form_y-0.5,
+				c,
 			})
-		end, {
-			class = classes.get_name(player) or "knight",
-			_on_formspec_input = function(pname, context, fields)
-				if fields.prev_class then
-					local classidx = table.indexof(class_list, context.class) - 1
+			table.insert(out,
+				{"tooltip[show_%s;Click to show class info]", c}
+			)
+		end
+		return ctf_gui.list_to_formspec_str(out)
+	end, {
+		classes = classes,
+		player = player,
+		pteam = pteam,
+		wrap_class = wrap_class,
+		class_list = class_list,
+		class_props = class_props,
+		class = classes.get_name(player) or "knight",
+		_on_formspec_input = function(pname, context, fields)
+			if ctf_modebase.current_mode ~= "classes" then return end
 
-					if classidx < 1 then
-						classidx = #class_list
+			if cooldowns:get(player) then
+				for _, class in pairs(context.class_list) do
+					if fields["select_"..class] then
+						context.class = class
+						return "refresh"
 					end
-
-					context.class = class_list[classidx]
-
+				end
+			end
+			for _, class in pairs(context.class_list) do
+				if fields["show_"..class] then
+					context.class = class
 					return "refresh"
-				elseif fields.next_class then
-					local classidx = table.indexof(class_list, context.class) + 1
+				end
 
-					if classidx > #class_list then
-						classidx = 1
-					end
-
-					context.class = class_list[classidx]
-
-					return "refresh"
-				elseif fields.select and classes.get_name(player) ~= context.class then
-					if ctf_modebase.current_mode ~= "classes" then
-						return
-					end
-
+				if fields["select_"..class] then
 					if dist_from_flag(player) > 5 then
 						hud_events.new(player, {
 							quick = true,
 							text = "You can only change class at your flag!",
 							color = "warning",
 						})
+
 						return
 					end
 
-					select_class(pname, context.class)
+					select_class(pname, class)
 				end
-			end,
-		})
-	else
-		hud_events.new(player, {
-			quick = true,
-			text = "You can only change your class every "
-				.. CLASS_SWITCH_COOLDOWN
-				.. " seconds",
-			color = "warning",
-		})
-	end
+			end
+		end,
+	})
 end
 
 function classes.is_restricted_item(player, name)
@@ -667,12 +664,29 @@ function classes.is_restricted_item(player, name)
 	end
 end
 
+function classes.reset_class_cooldowns(player)
+	if not player then
+		minetest.log("action", "Resetting class cooldowns for all players")
+
+		for _, p in pairs(minetest.get_connected_players()) do
+			if cooldowns:get(p) then
+				cooldowns:set(p)
+			end
+		end
+	else
+		minetest.log("action", "Resetting class cooldowns for player "..dump(PlayerName(player)))
+
+		if cooldowns:get(player) then
+			cooldowns:set(player)
+		end
+	end
+end
+
 function classes.finish()
-	for _, player in pairs(core.get_connected_players()) do
-		player:set_properties({
-			hp_max = core.PLAYER_MAX_HP_DEFAULT,
-			visual_size = vector.new(1, 1, 1),
-		})
+	for _, player in pairs(minetest.get_connected_players()) do
+		classes.reset_class_cooldowns()
+
+		player:set_properties({hp_max = minetest.PLAYER_MAX_HP_DEFAULT, visual_size = vector.new(1, 1, 1)})
 		physics.remove(player:get_player_name(), "ctf_mode_classes:class_physics")
 	end
 end
