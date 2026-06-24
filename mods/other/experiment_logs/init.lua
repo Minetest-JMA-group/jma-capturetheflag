79aee6213554ed25606d34b886c4c07db0968fca
Farooq Karimi Zadeh <fkz@riseup.net>
2026-06-24 15:19:23 +0330

    [other/experiment_logs] .log method accepts different targets(specific player, all players, team players)

--- mods/other/experiment_logs/init.lua
+++ mods/other/experiment_logs/init.lua
@@ -41,9 +41,9 @@ end
 --- experiment logs
 --- @param experiment_name string
 --- @param text string
---- @param only_team_players boolean?
+--- @param target ("|team_players|" | "|all|" | string)?
 --- @return number
-function experiment_logs.log(experiment_name, text, only_team_players)
+function experiment_logs.log(experiment_name, text, target)
 	if not experiments[experiment_name] then
 		core.log(
 			"warning",
@@ -56,20 +56,37 @@ function experiment_logs.log(experiment_name, text, only_team_players)
 	end
 	local experiment_setting = "experiment_logs:" .. experiment_name
 	local players = nil
-	if only_team_players then
+	if target == "|team_players|" then
 		players = ctf_teams.get_all_team_players()
-	else
+	elseif target == "|all|" or target == nil then
 		players = core.get_connected_players()
+	elseif type(target) == "table" then
+		players = target
+	elseif type(target) == "string" then
+		players = { target }
+	else
+		core.log("warning", "In experiment_logs.log, target has an invalid type or value")
+		return -1
 	end
 	local number_of_sent = 0
 	for _, player in ipairs(players) do
-		local pname = player:get_player_name()
+		local pname = PlayerName(player)
+		local master = ctf_settings.get(pname, "experiment_logs:subscribed")
+		local this = ctf_settings.get(pname, experiment_setting)
+		core.debug(
+			string.format(
+				"Player %s has got these values for experiment logs (master, this): %s %s",
+				pname,
+				master,
+				this
+			)
+		)
 		if
 			ctf_settings.get(pname, "experiment_logs:subscribed")
 			and ctf_settings.get(pname, experiment_setting)
 		then
 			number_of_sent = number_of_sent + 1
-			core.chat_send_player(S("[Experiment] ") .. text)
+			core.chat_send_player(pname, S("[Experiment] ") .. text)
 		end
 	end
 	return number_of_sent
